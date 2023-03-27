a96d1b4c0bd27f934743e834bc40778cfe9870ac
astrid <astridli@easyop.cn>
2023-03-27 11:07:08 +0800

    feat(): support battery-chart component
    refs DASHBOARD-235

--- bricks/data-view/src/bootstrap.ts
+++ bricks/data-view/src/bootstrap.ts
@@ -3,3 +3,5 @@ import "./modern-style-component-title/index.js";
 import "./modern-style-page-title/index.js";
 import "./tabs-drawer/index.js";
 import "./modern-style-treemap/index.js";
+import "./battery-chart/index.js";
+
