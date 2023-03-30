64a9c3492d9f045231600de43e636b9463c41fbd
weareoutman <wangshenwei@qq.com>
2023-03-30 17:33:09 +0800

    chore(): fix build config

--- bricks/icons/build.config.js
+++ bricks/icons/build.config.js
@@ -1,3 +1,31 @@
 // @ts-check
+import { getSvgrLoaders } from "@next-core/build-next-bricks";
+
+/**
+ * @param {string} input
+ * @returns {boolean}
+ */
+function issuer(input) {
+ // The issuer is null (or an empty string) for dynamic import
+ return !input || /\.[jt]sx?$/.test(input);
+}
+
 /** @type {import("@next-core/build-next-bricks").BuildNextBricksConfig} */
-export default {};
+export default {
+  svgRules: [{
+    resource: {
+      and: [
+        /\.svg$/i,
+        {
+          not: /\/colored-(?:pseudo-3d|common|big-screen)\/[^/]+\.svg$/i,
+        }
+      ]
+    },
+    issuer,
+    use: getSvgrLoaders({ convertCurrentColor: true}),
+  },{
+    test: /\/colored-(?:pseudo-3d|common|big-screen)\/[^/]+\.svg$/i,
+    issuer,
+    use: getSvgrLoaders({ convertCurrentColor: false }),
+  }]
+};
