ea8b622190bdc026f3142d6a80b7684c222867dc
weareoutman <wangshenwei@qq.com>
2023-03-31 16:20:40 +0800

    feat(): support setting gradient color

--- bricks/icons/src/antd-icon/index.tsx
+++ bricks/icons/src/antd-icon/index.tsx
@@ -1,26 +1,33 @@
 import React, { Suspense, lazy } from "react";
 import { createDecorators } from "@next-core/element";
 import { ReactNextElement, wrapLocalBrick } from "@next-core/react-element";
+import { DefineLinearGradient, DefineLinearGradientProps, GradientDirection } from "../shared/DefineLinearGradient.js";
+import linearGradientStyleText from "../shared/DefineLinearGradient.shadow.css";
 
 const { defineElement, property } = createDecorators();
 
-export interface AntdIconProps {
+export interface AntdIconProps extends DefineLinearGradientProps {
   /** Defaults to "outlined" */
   theme?: string;
   icon?: string;
 }
 
-@defineElement("icons.antd-icon")
+@defineElement("icons.antd-icon", {
+  styleTexts: [linearGradientStyleText]
+})
 class AntdIcon extends ReactNextElement implements AntdIconProps {
   @property() accessor theme: string | undefined;
   @property() accessor icon: string | undefined;
+  @property() accessor startColor: string | undefined;
+  @property() accessor endColor: string | undefined;
+  @property() accessor gradientDirection: GradientDirection | undefined;
 
   render() {
-    return <AntdIconComponent theme={this.theme} icon={this.icon} />;
+    return <AntdIconComponent theme={this.theme} icon={this.icon} startColor={this.startColor} endColor={this.endColor} gradientDirection={this.gradientDirection} />;
   }
 }
 
-function AntdIconComponent({ theme: _theme, icon }: AntdIconProps) {
+function AntdIconComponent({ theme: _theme, icon, startColor, endColor, gradientDirection }: AntdIconProps) {
   const theme = _theme ?? "outlined";
 
   if (!icon) {
@@ -38,6 +45,7 @@ function AntdIconComponent({ theme: _theme, icon }: AntdIconProps) {
   return (
     <Suspense>
       <Icon />
+      <DefineLinearGradient startColor={startColor} endColor={endColor} gradientDirection={gradientDirection} />
     </Suspense>
   );
 }
