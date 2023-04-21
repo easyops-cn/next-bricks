7cb293b6d7dc1b76e481147ee632adecc6fdacd7
astrid <astridli@easyop.cn>
2023-04-21 19:16:22 +0800

    fix(cabinet-container): 增加叠层&布局由于clientWidth四舍五入，导致布局不对
    refs DASHBOARD-292

--- bricks/data-view/src/cabinet/cabinet-container/index.tsx
+++ bricks/data-view/src/cabinet/cabinet-container/index.tsx
@@ -85,12 +85,19 @@ export function CabinetContainerComponent(props: CabinetContainerProps): React.R
     const [containerRef, {clientWidth, clientHeight}] = useResizeObserver<HTMLDivElement>();
     const binarySearch = (maxSizeCol: number, minSizeCol: number, maxSizeRow: number, minSizeRow: number): { col: number, row: number } => {
         // 利用二分法，去找到一个合适大小；
-        // maxSizeCol < minSizeCol  maxSizeRow< minSizeRow
+        const maxRow = Math.max(minSizeRow,maxSizeRow),
+            minRow = Math.min(minSizeRow, minSizeRow),
+            minCol = Math.min(minSizeCol, maxSizeCol),
+            maxCol = Math.max(minSizeCol, maxSizeCol);
         const centerCol = Math.floor((maxSizeCol+minSizeCol)/2);
         const centerRow = Math.ceil((maxSizeRow+minSizeRow) /2);
         const needRow = Math.ceil(data.length /centerCol);
-        const maxRow = Math.max(needRow,maxSizeRow),
-            minRow = Math.min(needRow, minSizeRow);
+        if(centerCol <= 1){
+            return {
+                col: 1,
+                row: data.length
+            }
+        }
         if(needRow === centerRow) {
             return {
                 col: centerCol,
@@ -98,12 +105,12 @@ export function CabinetContainerComponent(props: CabinetContainerProps): React.R
             }
         }
         if(needRow > centerRow) {
-            // 那么col要放大，
-            return  binarySearch(centerCol ,minSizeCol,maxSizeRow, minRow)
+            // 那么col要放大，row减少
+            return  binarySearch(centerCol ,maxCol,needRow, minRow)
         }
         if(needRow < centerRow) {
-            // 那么col要缩小，
-            return  binarySearch(maxSizeCol,minSizeCol,maxRow, minRow)
+            // 那么col要缩小，row增加
+            return  binarySearch(minCol,centerCol,maxRow, needRow)
         }
     }
     const layoutWidth = useMemo(() => {
@@ -116,8 +123,11 @@ export function CabinetContainerComponent(props: CabinetContainerProps): React.R
         // C>=A: 用最小尺寸
         // B< A <C: 反推尺寸,
         const maxWidth = 92, minWidth = 56, maxHeight = 105, minHeight = 63;
-        const realWidth = clientWidth - 26;
-        const realHeight = clientHeight - 83;
+        const realWidth = clientWidth - 27;
+        const realHeight =clientHeight - 84;
+        if(realWidth <=0 ||realHeight<=0 ){
+            return
+        }
         const maxSizeCol = Math.floor(realWidth / maxWidth),
             maxSizeRow = Math.floor(realHeight / maxHeight),
             minSizeCol = Math.floor(realWidth / minWidth),
@@ -127,32 +137,36 @@ export function CabinetContainerComponent(props: CabinetContainerProps): React.R
         const allNum = data.length;
         if (allNum <= maxSizeNum) {
             return {
-                width: realWidth / maxSizeCol,
+                width: Math.floor(realWidth / maxSizeCol),
                 itemWidth: maxWidth
             };
         } else if (allNum >= minSizeNum) {
             return {
-                width: realWidth / minSizeCol,
+                width: Math.floor(realWidth / minSizeCol),
                 itemWidth: minWidth
             };
         } else {
             // 在区间内， 希望撑满整个屏幕, 尺寸肯定在最大和最小之间
             const {col} = binarySearch(maxSizeCol, minSizeCol, maxSizeRow, minSizeRow);
             return {
-                width: realWidth / col,
-                itemWidth: Math.max(realWidth / col - 10, minWidth),
+                width: Math.floor(realWidth / col),
+                itemWidth: Math.max(realWidth / col - 10, minWidth)
             };
         }
 
     }, [data.length, clientHeight, clientWidth])
 
-    return <div className="wrapper" ref={containerRef}>
+    return <div className={classNames("wrapper",{
+        hostWrapper: type === "host",
+        k8sWrapper: type === "k8s",
+    },status && `wrapper-${status}`)} ref={containerRef}>
         <div className={classNames("container", {
             hostContainer: type === "host",
             k8sContainer: type === "k8s",
         }, status && `container-${status}`)}
         >
             <div className="content">
+                <div className="contentLayout">
                     {
                         data.map((item, index) => (
                             <div key={index} className="itemContent" style={{width:layoutWidth?.width}}>
@@ -161,6 +175,7 @@ export function CabinetContainerComponent(props: CabinetContainerProps): React.R
                                 </div>
                             </div>))
                     }
+                </div>
             </div>
             {customTitle && <div className="footer">{customTitle}</div>}
         </div>
