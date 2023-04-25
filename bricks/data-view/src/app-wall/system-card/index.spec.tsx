6126e63976815c5d7007611bfb736a9a82639d8a
nlicroshan <nlicroshan@easyops.cn>
2023-04-25 22:52:30 +0800

    fix(): 系统卡片更新滚动条需重置回顶部
    Refs: DASHBOARD-322

--- bricks/data-view/src/app-wall/system-card/index.spec.tsx
+++ bricks/data-view/src/app-wall/system-card/index.spec.tsx
@@ -1,53 +1,60 @@
 import { describe, test, expect, jest } from "@jest/globals";
-import React from "react";
 import { act } from "react-dom/test-utils";
 import "./index.js";
-import { render ,fireEvent} from "@testing-library/react";
-import  {SystemCard,SystemCardComponent} from "./index.js";
-describe("data-view.app-wall-system-card", () => {
-    test("basic usage" , async ()=>{
-        const element = document.createElement("data-view.app-wall-system-card") as SystemCard;
-        const mockClickFn = jest.fn();
-        expect(element.shadowRoot).toBeFalsy();
-        const  itemList = [{
-            key: "name",
-            value: "test"
-        }]
+import { fireEvent } from "@testing-library/react";
+import { SystemCard } from "./index.js";
 
-       act (()=>{
-            element.cardTitle = "优维科技";
-            element.itemList = itemList;
-            element.buttonName= "测试";
-            document.body.appendChild(element);
-        });
-        expect(element.shadowRoot).toBeTruthy();
-        const btnNode = element.shadowRoot.querySelector(".buttonContent");
-        expect(element.shadowRoot.querySelector(".buttonContent").childNodes.length).toBe(2);
-        fireEvent.click(btnNode);
-        expect(element.shadowRoot.querySelector(".cardName").innerHTML).toBe("优维科技");
-        const {container,rerender} = await act(async ()=>
-            render(<SystemCardComponent
-                status="normal"
-                cardTitle="卡片标题"
-                itemList={itemList}
-                handleClick={mockClickFn}
-                buttonName="应用墙大屏" />));
-        expect(container.querySelector(".wrapper").classList).toContain("infoWrapper");
-        const btn = container.querySelector(".buttonContent");
-        await act(async ()=>{
-            fireEvent.click(btn);
-        })
-        expect(mockClickFn).toHaveBeenCalled();
-        rerender(<SystemCardComponent status="warning" cardTitle="卡片标题" itemList={itemList} containerStyle={{
-            width: "500px",
-            height: "600px"
-        }} />);
-        expect(container.querySelector(".buttonContent")).toBeNull();
-        expect(container.querySelector(".wrapper").classList).toContain("warningWrapper");
-        expect(container.querySelector(".wrapper").getAttribute("style")).toBe("width: 500px; height: 600px;");
-        act(() => {
-            document.body.removeChild(element);
-        });
-        expect(document.body.contains(element)).toBeFalsy();
-    })
+describe("data-view.app-wall-system-card", () => {
+  test("basic usage", async () => {
+    const element = document.createElement("data-view.app-wall-system-card") as SystemCard;
+    const mockClickFn = jest.fn();
+    const itemList = [{
+      key: "name",
+      value: "test"
+    }];
+    element.cardTitle = "优维科技";
+    element.itemList = itemList;
+    element.buttonName = "测试";
+    element.status = "normal";
+    element.addEventListener("button-click", mockClickFn);
+
+    expect(element.shadowRoot).toBeFalsy();
+    act(() => {
+      document.body.appendChild(element);
+    });
+    expect(element.shadowRoot).toBeTruthy();
+
+    expect(element.shadowRoot.querySelector(".wrapper").classList).toContain("infoWrapper");
+    expect(element.shadowRoot.querySelector(".buttonContent").childNodes.length).toBe(2);
+    expect(element.shadowRoot.querySelector(".cardName").textContent).toBe("优维科技");
+
+    act(() => {
+      fireEvent.click(element.shadowRoot.querySelector(".buttonContent"));
+    });
+    expect(mockClickFn).lastCalledWith(expect.objectContaining({
+      type: "button-click",
+      detail: null,
+    }));
+
+    await act(async () => {
+      element.status = "warning";
+      element.buttonName = null;
+      element.containerStyle = {
+        width: "500px",
+        height: "600px"
+      };
+    });
+
+    expect(element.shadowRoot.querySelector(".buttonContent")).toBeNull();
+    expect(element.shadowRoot.querySelector(".wrapper").classList).toContain("warningWrapper");
+    expect((element.shadowRoot.querySelector(".wrapper") as HTMLElement).style).toEqual(expect.objectContaining({
+      width: "500px",
+      height: "600px"
+    }));
+
+    act(() => {
+      document.body.removeChild(element);
+    });
+    expect(document.body.contains(element)).toBeFalsy();
+  })
 })
--- bricks/data-view/src/app-wall/system-card/index.tsx
+++ bricks/data-view/src/app-wall/system-card/index.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import {ReactNextElement, wrapBrick} from "@next-core/react-element";
-import {createDecorators, EventEmitter} from "@next-core/element";
+import React, { useEffect, useRef } from "react";
+import { ReactNextElement, wrapBrick } from "@next-core/react-element";
+import { createDecorators, EventEmitter } from "@next-core/element";
 import styleText from "./system-card.shadow.css";
 import classNames from "classnames";
 import variablesStyleText from "../../data-view-variables.shadow.css";
 import type {
-    GeneralIcon,
-    GeneralIconProps
+  GeneralIcon,
+  GeneralIconProps
 } from "@next-bricks/icons/general-icon";
 
-const {defineElement, property, event} = createDecorators();
+const { defineElement, property, event } = createDecorators();
 const WrappedIcon = wrapBrick<GeneralIcon, GeneralIconProps>(
-    "icons.general-icon"
+  "icons.general-icon"
 );
 type StatusType = "normal" | "warning"
 interface DescriptionItem {
-    key: string;
-    value: string;
+  key: string;
+  value: string;
 
 }
 export interface SystemCardProps {
-    status: StatusType;
-    cardTitle: string|undefined;
-    itemList?: DescriptionItem[];
-    buttonName?: string;
-    handleClick?: ()=>void;
-    containerStyle?: React.CSSProperties;
+  status: StatusType;
+  cardTitle: string | undefined;
+  itemList?: DescriptionItem[];
+  buttonName?: string;
+  handleClick?: () => void;
+  containerStyle?: React.CSSProperties;
 }
 
 /**
@@ -37,116 +37,122 @@ export interface SystemCardProps {
  * @noInheritDoc
  */
 @defineElement("data-view.app-wall-system-card", {
-    styleTexts: [variablesStyleText, styleText],
+  styleTexts: [variablesStyleText, styleText],
 })
 class SystemCard extends ReactNextElement implements SystemCardProps {
-    /**
-     * @kind StatusType
-     * @required true
-     * @default normal
-     * @description 卡片状态
-     * @enums
-     * @group basic
-     */
-    @property({attribute: false})
-    accessor status: StatusType = "normal";
+  /**
+   * @kind StatusType
+   * @required true
+   * @default normal
+   * @description 卡片状态
+   * @enums
+   * @group basic
+   */
+  @property({ attribute: false })
+  accessor status: StatusType = "normal";
 
-    /**
-     * @kind string
-     * @required -
-     * @default -
-     * @description 卡片标题
-     * @enums
-     * @group basic
-     */
-    @property()
-    accessor cardTitle:string|undefined;
+  /**
+   * @kind string
+   * @required -
+   * @default -
+   * @description 卡片标题
+   * @enums
+   * @group basic
+   */
+  @property()
+  accessor cardTitle: string | undefined;
 
-    /**
-     * @kind DescriptionItem[]
-     * @required false
-     * @default -
-     * @description 卡片信息数据
-     * @enums
-     * @group basic
-     */
-    @property({attribute: false})
-    accessor itemList: DescriptionItem[];
+  /**
+   * @kind DescriptionItem[]
+   * @required false
+   * @default -
+   * @description 卡片信息数据
+   * @enums
+   * @group basic
+   */
+  @property({ attribute: false })
+  accessor itemList: DescriptionItem[];
 
-    /**
-     * @kind string
-     * @required -
-     * @default -
-     * @description 按钮名称
-     * @enums
-     * @group basic
-     */
-    @property()
-    accessor buttonName:string|undefined;
+  /**
+   * @kind string
+   * @required -
+   * @default -
+   * @description 按钮名称
+   * @enums
+   * @group basic
+   */
+  @property()
+  accessor buttonName: string | undefined;
 
-    /**
-     * @kind React.CSSProperties
-     * @required -
-     * @default -
-     * @description 外层容器样式
-     * @enums
-     * @group basic
-     */
-    @property({attribute: false})
-    accessor containerStyle: React.CSSProperties;
+  /**
+   * @kind React.CSSProperties
+   * @required -
+   * @default -
+   * @description 外层容器样式
+   * @enums
+   * @group basic
+   */
+  @property({ attribute: false })
+  accessor containerStyle: React.CSSProperties;
 
-    /**
-     * @detail
-     * @description 按钮点击
-     */
-    @event({ type: "button-click" })
-    accessor #onClickEvent!: EventEmitter<void>;
+  /**
+   * @detail
+   * @description 按钮点击
+   */
+  @event({ type: "button-click" })
+  accessor #onClickEvent!: EventEmitter<void>;
 
-    handleClick = ()=>{
-        this.#onClickEvent.emit();
-    }
+  handleClick = () => {
+    this.#onClickEvent.emit();
+  }
 
 
-    render(): React.ReactNode {
-        return <SystemCardComponent
-            buttonName={this.buttonName}
-            status={this.status}
-            cardTitle={this.cardTitle}
-            itemList={this.itemList}
-            handleClick={this.handleClick}
-            containerStyle={this.containerStyle}
-        />;
-    }
+  render(): React.ReactNode {
+    return <SystemCardComponent
+      buttonName={this.buttonName}
+      status={this.status}
+      cardTitle={this.cardTitle}
+      itemList={this.itemList}
+      handleClick={this.handleClick}
+      containerStyle={this.containerStyle}
+    />;
+  }
 }
 
 export function SystemCardComponent(props: SystemCardProps): React.ReactElement {
-    const {status,itemList,cardTitle, buttonName ,handleClick, containerStyle}=props;
-    return (
-            <div className={classNames("wrapper", {
-                infoWrapper: status === "normal",
-                warningWrapper: status === "warning"
-            })} style={containerStyle}>
-              <div className="cardName">{cardTitle}</div>
-                <div className="descriptions">
-                    {
-                        itemList?.map((item,index) => (
-                            <div key={index} className="descriptionsItem">
-                                <div className="itemKey">{item.key}</div>
-                                <div className="itemValue">{item.value}</div>
-                            </div>))
-                    }
-                </div>
-                {
-                    buttonName &&
-                    <div className="buttonContent" onClick={handleClick}>
-                        <WrappedIcon lib="antd" icon="fall" theme="outlined"/>
-                        <span className="buttonName">{buttonName}</span>
-                    </div>
-                }
+  const { status, itemList, cardTitle, buttonName, handleClick, containerStyle } = props;
+  const descriptionRef = useRef<HTMLDivElement>();
 
+  useEffect(() => {
+    descriptionRef.current?.scrollTo?.(0, 0);
+  }, [itemList]);
 
-            </div>
-        )
+  return (
+    <div className={classNames("wrapper", {
+      infoWrapper: status === "normal",
+      warningWrapper: status === "warning"
+    })} style={containerStyle}>
+      <div className="cardName" title={cardTitle}>{cardTitle}</div>
+      {
+        itemList?.length && (<div className="descriptions" ref={descriptionRef}>
+          {
+            itemList.map((item, index) => (
+              <div key={index} className="descriptionsItem">
+                <div className="itemKey">{item.key}</div>
+                <div className="itemValue">{item.value}</div>
+              </div>))
+          }
+        </div>)
+      }
+      {
+        buttonName &&
+        <div className="buttonContent" onClick={handleClick}>
+          <WrappedIcon lib="antd" icon="fall" theme="outlined" />
+          <span className="buttonName">{buttonName}</span>
+        </div>
+      }
+    </div>
+  )
 }
 
-export {SystemCard}
+export { SystemCard }
