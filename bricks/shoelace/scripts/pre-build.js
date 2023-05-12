b4f35ec6a8d1b8b37ed051a5d7c5bbbce3807c25
weareoutman <wangshenwei@qq.com>
2023-05-12 16:30:04 +0800

    feat(): add utilities

--- bricks/shoelace/scripts/pre-build.js
+++ bricks/shoelace/scripts/pre-build.js
@@ -10,17 +10,11 @@ const elements = [
   // "alert",
   "avatar",
   "badge",
-  [
-    "breadcrumb",
-    "breadcrumb-item",
-  ],
+  ["breadcrumb", "breadcrumb-item"],
   // "button",
   "button-group",
   "card",
-  [
-    "carousel",
-    "carousel-item",
-  ],
+  ["carousel", "carousel-item"],
   "checkbox",
   "color-picker",
   "details",
@@ -32,10 +26,7 @@ const elements = [
   // "icon-button",
   "image-comparer",
   "input",
-  [
-    "menu",
-    "menu-item",
-  ],
+  ["menu", "menu-item"],
   "menu-label",
   "option",
   "progress-bar",
@@ -51,18 +42,21 @@ const elements = [
   "spinner",
   "split-panel",
   "switch",
-  [
-    "tab-group",
-    "tab",
-    "tab-panel",
-  ],
+  ["tab-group", "tab", "tab-panel"],
   "tag",
   "textarea",
   "tooltip",
-  [
-    "tree",
-    "tree-item",
-  ],
+  ["tree", "tree-item"],
+  "animated-image",
+  "animation",
+  "format-bytes",
+  "format-date",
+  "format-number",
+  "mutation-observer",
+  "popup",
+  "relative-time",
+  "resize-observer",
+  "visually-hidden",
 ];
 
 const generatedDir = path.resolve(__dirname, "../src/generated");
@@ -71,27 +65,36 @@ if (!existsSync(generatedDir)) {
   mkdirSync(generatedDir);
 }
 
-await Promise.all(elements.map((element) => {
-  /** @type {string} */
-  let filePath;
-  /** @type {string} */
-  let content;
-  if (Array.isArray(element)) {
-    filePath = path.join(generatedDir, `${element[0]}.ts`);
-    content = `// Define bricks: ${element.map(e => `sl-${e}`).join(", ")}
-${element.map(e => `import "@shoelace-style/shoelace/dist/components/${e}/${e}.js";`).join("\n")}
+await Promise.all(
+  elements.map((element) => {
+    /** @type {string} */
+    let filePath;
+    /** @type {string} */
+    let content;
+    if (Array.isArray(element)) {
+      filePath = path.join(generatedDir, `${element[0]}.ts`);
+      content = `// Define bricks: ${element.map((e) => `sl-${e}`).join(", ")}
+${element
+  .map((e) => `import "@shoelace-style/shoelace/dist/components/${e}/${e}.js";`)
+  .join("\n")}
 import "../shared/common.js";
 `;
-  } else {
-    filePath = path.join(generatedDir, `${element}.ts`);
-    content = `// Define brick: sl-${element}
+    } else {
+      filePath = path.join(generatedDir, `${element}.ts`);
+      content = `// Define brick: sl-${element}
 import "@shoelace-style/shoelace/dist/components/${element}/${element}.js";
 import "../shared/common.js";
 `;
-  }
-  return writeFile(filePath, content);
-}));
+    }
+    return writeFile(filePath, content);
+  })
+);
 
 const indexPath = path.join(generatedDir, "index.ts");
-const indexContent = elements.map((element) => `import "./${Array.isArray(element) ? element[0] : element}.js";`).join("\n");
+const indexContent = elements
+  .map(
+    (element) =>
+      `import "./${Array.isArray(element) ? element[0] : element}.js";`
+  )
+  .join("\n");
 await writeFile(indexPath, indexContent);
