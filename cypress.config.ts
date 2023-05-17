2207e05d0ac0c1a7079aa964b4176b685a58dc60
weareoutman <wangshenwei@qq.com>
2023-05-17 11:06:42 +0800

    chore(): add size check

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "m3skph",
+  videoUploadOnPasses: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    specPattern: "cypress/size-check.spec.js",
+  },
+});
