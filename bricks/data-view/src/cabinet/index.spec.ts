e615417e778a489efc1782e90d58c29aec391196
astrid <astridli@easyop.cn>
2023-05-18 15:48:22 +0800

    feat(data-view): cabinet add dbclick event
    refs DASHBOARD-403

--- bricks/data-view/src/cabinet/index.spec.ts
+++ bricks/data-view/src/cabinet/index.spec.ts
@@ -1,4 +1,4 @@
-import { fireEvent } from '@testing-library/react';
+import { fireEvent } from "@testing-library/react";
 import { describe, test, expect } from "@jest/globals";
 import { act } from "react-dom/test-utils";
 import "./cabinet-app-layer/index.jsx";
@@ -9,7 +9,7 @@ import type { CabinetAppLayer } from "./cabinet-app-layer/index.jsx";
 import type { CabinetContainer } from "./cabinet-container/index.jsx";
 import "./index.jsx";
 import { CabinetGraph } from "./index.jsx";
-
+jest.useFakeTimers();
 describe("data-view.cabinet-graph", () => {
   test("basic usage", async () => {
     const element = document.createElement(
@@ -18,50 +18,50 @@ describe("data-view.cabinet-graph", () => {
     const onCloseBtnClick = jest.fn();
 
     element.dataSource = {
-      "appName": "inventory-api",
-      "key": "inventory-api",
-      "clusters": [
+      appName: "inventory-api",
+      key: "inventory-api",
+      clusters: [
         {
-          "clusterName": "inventory-api##aaaaa",
-          "key": "inventory-api##aaaaa",
-          "type": "host",
-          "nodes": [
+          clusterName: "inventory-api##aaaaa",
+          key: "inventory-api##aaaaa",
+          type: "host",
+          nodes: [
             {
-              "nodeTitle": "244.244.244.244",
-              "key": "244.244.244.244",
-              "type": "physical-machine"
+              nodeTitle: "244.244.244.244",
+              key: "244.244.244.244",
+              type: "physical-machine",
             },
             {
-              "nodeTitle": "244.244.244.245",
-              "key": "244.244.244.245",
-              "type": "virtual-machine"
-            }
-          ]
+              nodeTitle: "244.244.244.245",
+              key: "244.244.244.245",
+              type: "virtual-machine",
+            },
+          ],
         },
         {
-          "clusterName": "K8S集群",
-          "key": "k8s-cluster",
-          "type": "k8s",
-          "nodes": [
+          clusterName: "K8S集群",
+          key: "k8s-cluster",
+          type: "k8s",
+          nodes: [
             {
-              "nodeTitle": "a容器组",
-              "key": "aa",
-              "type": "container-group"
+              nodeTitle: "a容器组",
+              key: "aa",
+              type: "container-group",
             },
             {
-              "nodeTitle": "b容器组",
-              "key": "bb",
-              "type": "container-group"
+              nodeTitle: "b容器组",
+              key: "bb",
+              type: "container-group",
             },
             {
-              "nodeTitle": "c容器组",
-              "key": "cc",
-              "type": "container-group"
+              nodeTitle: "c容器组",
+              key: "cc",
+              type: "container-group",
             },
-          ]
+          ],
         },
-      ]
-    }
+      ],
+    };
     element.addEventListener("close.button.click", onCloseBtnClick);
 
     expect(element.shadowRoot).toBeFalsy();
@@ -73,37 +73,55 @@ describe("data-view.cabinet-graph", () => {
     await act(async () => {
       fireEvent.click(element.shadowRoot.querySelector(".app-layer"));
     });
-    expect((element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status).toBe("active")
+    expect(
+      (element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status
+    ).toBe("active");
 
     await act(async () => {
       element.activeKey = null;
     });
-    expect((element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status).toBe(null);
-
+    expect(
+      (element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status
+    ).toBe(null);
+    const clusterContainerEle =
+      element.shadowRoot.querySelector(".cluster-container");
     await act(async () => {
-      const cluster = element.shadowRoot.querySelector(".cluster-container").children[0].shadowRoot.querySelector(".wrapper");
+      const cluster =
+        clusterContainerEle.children[0].shadowRoot.querySelector(".wrapper");
       fireEvent.click(cluster);
+      jest.advanceTimersByTime(400);
+      fireEvent.doubleClick(cluster);
     });
-    expect((element.shadowRoot.querySelector(".cluster-container").children[0] as CabinetContainer).status).toBe("active");
-    expect((element.shadowRoot.querySelector(".cluster-container").children[1] as CabinetContainer).status).toBe("faded");
-
+    expect((clusterContainerEle.children[0] as CabinetContainer).status).toBe(
+      "active"
+    );
+    expect((clusterContainerEle.children[1] as CabinetContainer).status).toBe(
+      "faded"
+    );
     await act(async () => {
-      const node = element.shadowRoot.querySelector(".cluster-container").children[0].shadowRoot.querySelector(".item");
+      const node =
+        clusterContainerEle.children[0].shadowRoot.querySelector(".item");
       fireEvent.click(node);
+      jest.advanceTimersByTime(400);
+      fireEvent.doubleClick(node);
     });
 
     await act(async () => {
       fireEvent.click(element.shadowRoot.querySelector(".close-button"));
     });
-    expect(onCloseBtnClick).lastCalledWith(expect.objectContaining({
-      type: "close.button.click",
-      detail: null,
-    }));
+    expect(onCloseBtnClick).lastCalledWith(
+      expect.objectContaining({
+        type: "close.button.click",
+        detail: null,
+      })
+    );
 
     await act(async () => {
       fireEvent.click(element.shadowRoot.querySelector(".wrapper"));
     });
-    expect((element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status).toBe(null);
+    expect(
+      (element.shadowRoot.querySelector(".app-layer") as CabinetAppLayer).status
+    ).toBe(null);
 
     act(() => {
       document.body.removeChild(element);
