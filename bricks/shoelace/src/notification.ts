116d1446ca8695e70ed57c1db01edac9cb2f49a3
weareoutman <wangshenwei@qq.com>
2023-06-16 16:22:34 +0800

    fix(): no merge for former notification bricks

--- bricks/shoelace/src/notification.ts
+++
@@ -1,8 +0,0 @@
-// Define brick: sl-alert, sl-button, sl-dialog, sl-icon, sl-icon-button
-// istanbul ignore file
-import "@shoelace-style/shoelace/dist/components/alert/alert.js";
-import "@shoelace-style/shoelace/dist/components/button/button.js";
-import "@shoelace-style/shoelace/dist/components/dialog/dialog.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js";
-import "./shared/common.js";
