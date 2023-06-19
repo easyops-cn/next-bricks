ea9efb4f2c3c3c07ce0e80e49bf7197e18ad5cc2
astrid <astridli@easyop.cn>
2023-06-19 11:56:17 +0800

    feat(): tabs-drawer点击非自身元素收起
    Closes DASHBOARD-452

--- bricks/data-view/src/tabs-drawer/index.spec.tsx
+++ bricks/data-view/src/tabs-drawer/index.spec.tsx
@@ -1,85 +1,92 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect, jest } from "@jest/globals";
 import { act } from "react-dom/test-utils";
 import "./index.jsx";
-import {TabItem, TabsDrawer, TabsDrawerComponent} from "./index.jsx";
+import { TabItem, TabsDrawer, TabsDrawerComponent } from "./index.jsx";
 import { render, fireEvent } from "@testing-library/react";
 import React from "react";
-const tabList:TabItem[]= [
-    {
-        key: "search",
-        tooltip: "搜索",
-        icon: {
-            lib: "antd",
-            icon: "file-search",
-            theme: "outlined",
-        }
+const tabList: TabItem[] = [
+  {
+    key: "search",
+    tooltip: "搜索",
+    icon: {
+      lib: "antd",
+      icon: "file-search",
+      theme: "outlined",
     },
-    {
-        key: "app",
-        tooltip: "应用",
-        icon: {
-            lib: "easyops",
-            category: "app",
-            icon: "micro-app-configuration",
-        }
-    }
+  },
+  {
+    key: "app",
+    tooltip: "应用",
+    icon: {
+      lib: "easyops",
+      category: "app",
+      icon: "micro-app-configuration",
+    },
+  },
 ];
-describe("data-view.tabs-drawer", ()=>{
-    test("basic usage", async ()=>{
-        const element =  document.createElement("data-view.tabs-drawer") as TabsDrawer;
-        expect(element.shadowRoot).toBeFalsy();
-        element.tabList = tabList;
-        element.activeKey = "app";
-        act(()=>{
-            document.body.appendChild(element);
-        });
-        element.open();
-        expect(element.visible).toBe(true);
-       const tabElement= element.shadowRoot.querySelector(".menuIconItem");
-        fireEvent.click(tabElement);
+describe("data-view.tabs-drawer", () => {
+  test("basic usage", async () => {
+    const element = document.createElement(
+      "data-view.tabs-drawer"
+    ) as TabsDrawer;
+    const divEle = document.createElement("div");
+    divEle.textContent = "test";
+    expect(element.shadowRoot).toBeFalsy();
+    element.tabList = tabList;
+    element.activeKey = "app";
+    element.visible = true;
+    act(() => {
+      document.body.appendChild(element);
+      document.body.appendChild(divEle);
+    });
+    await act(() => {
+      fireEvent.click(divEle);
+    });
+    element.open();
+    expect(element.visible).toBe(true);
+    const tabElement = element.shadowRoot.querySelector(".menuIconItem");
+    fireEvent.click(tabElement);
 
-        element.close();
-        expect(element.visible).toBe(false);
-        expect(document.body.contains(element)).toBeTruthy();
-        expect(element.shadowRoot).toBeTruthy();
-        expect(element.shadowRoot?.childNodes.length).toBe(2);
-        act(() => {
-            document.body.removeChild(element);
-        });
-        expect(document.body.contains(element)).toBeFalsy();
-    })
-    test("change tab", async ()=>{
-        const openMockFn = jest.fn();
-        const closeMockFn = jest.fn();
-        const changeMockFn= jest.fn();
-        const {container,asFragment ,rerender} = render(
-            <TabsDrawerComponent
-                tabList={tabList}
-                activeKey="search"
-                onOpen={openMockFn}
-                onClose={closeMockFn}
-                width={500}
-                zIndex={999}
-                onTabChange={changeMockFn}
-            />
-        );
-        expect(asFragment()).toBeTruthy();
-        const closeBtn = container.querySelector(".closeIconBtn");
-        fireEvent.click(closeBtn);
-        expect(openMockFn).toBeCalledTimes(1);
-        expect(changeMockFn).toBeCalledTimes(0);
-       const menuItems  = container.querySelector(".menuIconItem");
-       expect(menuItems.childNodes.length).toBe(2);
-       const activeElement = menuItems.firstElementChild;
-       const noActiveElement = menuItems.lastElementChild;
-       fireEvent.click(noActiveElement);
-       expect(activeElement.classList.contains("active")).toBeFalsy();
-       expect(changeMockFn).toBeCalledTimes(1);
-       rerender(
-           <TabsDrawerComponent
-               tabList={tabList}
-           />
-       )
-        expect(asFragment()).toBeTruthy();
-    })
-})
+    element.close();
+    expect(element.visible).toBe(false);
+    expect(document.body.contains(element)).toBeTruthy();
+    expect(element.shadowRoot).toBeTruthy();
+    expect(element.shadowRoot?.childNodes.length).toBe(2);
+    act(() => {
+      document.body.removeChild(element);
+    });
+    expect(document.body.contains(element)).toBeFalsy();
+  });
+  test("change tab", async () => {
+    const openMockFn = jest.fn();
+    const closeMockFn = jest.fn();
+    const changeMockFn = jest.fn();
+    const { container, asFragment, rerender } = render(
+      <TabsDrawerComponent
+        tabList={tabList}
+        activeKey="search"
+        onOpen={openMockFn}
+        onClose={closeMockFn}
+        width={500}
+        zIndex={999}
+        onTabChange={changeMockFn}
+      />
+    );
+    expect(asFragment()).toBeTruthy();
+    const closeBtn = container.querySelector(".closeIconBtn");
+    fireEvent.click(closeBtn);
+    expect(openMockFn).toBeCalledTimes(1);
+    expect(changeMockFn).toBeCalledTimes(0);
+    fireEvent.click(closeBtn);
+    expect(closeMockFn).toBeCalledTimes(1);
+    const menuItems = container.querySelector(".menuIconItem");
+    expect(menuItems.childNodes.length).toBe(2);
+    const activeElement = menuItems.firstElementChild;
+    const noActiveElement = menuItems.lastElementChild;
+    fireEvent.click(noActiveElement);
+    expect(activeElement.classList.contains("active")).toBeFalsy();
+    expect(changeMockFn).toBeCalledTimes(1);
+    rerender(<TabsDrawerComponent tabList={tabList} />);
+    expect(asFragment()).toBeTruthy();
+  });
+});
--- bricks/data-view/src/tabs-drawer/index.tsx
+++ bricks/data-view/src/tabs-drawer/index.tsx
@@ -1,38 +1,43 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
-import {createDecorators, EventEmitter} from "@next-core/element";
-import {ReactNextElement, wrapBrick} from "@next-core/react-element";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { createDecorators, EventEmitter } from "@next-core/element";
+import { ReactNextElement, wrapBrick } from "@next-core/react-element";
 import type {
-    GeneralIcon,
-    GeneralIconProps,
+  GeneralIcon,
+  GeneralIconProps,
 } from "@next-bricks/icons/general-icon";
 import variablesStyleText from "../data-view-variables.shadow.css";
 import styleText from "./tabs-drawer.shadow.css";
 import classNames from "classnames";
-const { defineElement, property ,event, method} = createDecorators();
+const { defineElement, property, event, method } = createDecorators();
 const WrappedIcon = wrapBrick<GeneralIcon, GeneralIconProps>(
-    "icons.general-icon"
+  "icons.general-icon"
 );
 export interface TabItem {
-    /** 菜单标题对应的图标。 */
-    icon: GeneralIconProps;
-    /** 唯一标识 */
-    key: string;
-    /** 菜单标题。 */
-    title?: string;
-    tooltip?: string;
-
+  /** 菜单标题对应的图标。 */
+  icon: GeneralIconProps;
+  /** 唯一标识 */
+  key: string;
+  /** 菜单标题。 */
+  title?: string;
+  tooltip?: string;
 }
 interface TabsDrawerProps {
-    tabList: TabItem[];
-    activeKey?: string;
-    width?: number | string;
-    drawerStyle?: React.CSSProperties;
-    bodyStyle?: React.CSSProperties;
-    zIndex?: number;
-    visible?: boolean;
-    onClose?: () => void;
-    onOpen?: () => void
-    onTabChange?: (key:string)=>void;
+  tabList: TabItem[];
+  activeKey?: string;
+  width?: number | string;
+  drawerStyle?: React.CSSProperties;
+  bodyStyle?: React.CSSProperties;
+  zIndex?: number;
+  visible?: boolean;
+  onClose?: () => void;
+  onOpen?: () => void;
+  onTabChange?: (key: string) => void;
 }
 /**
  * @id data-view.tabs-drawer
@@ -43,219 +48,263 @@ interface TabsDrawerProps {
  * @noInheritDoc
  */
 
-@defineElement("data-view.tabs-drawer",{
-    styleTexts: [variablesStyleText,styleText]
+@defineElement("data-view.tabs-drawer", {
+  styleTexts: [variablesStyleText, styleText],
 })
 class TabsDrawer extends ReactNextElement implements TabsDrawerProps {
-    /**
-     * @default -
-     * @required false
-     * @description 抽屉左侧菜单列表
-     */
-    @property({attribute: false})
-    accessor tabList: TabItem[];
+  /**
+   * @default -
+   * @required false
+   * @description 抽屉左侧菜单列表
+   */
+  @property({ attribute: false })
+  accessor tabList: TabItem[];
 
-    /**
-     * @default -
-     * @required false
-     * @description 抽屉左侧菜单高亮显示
-     */
-    @property()
-    accessor activeKey: string;
+  /**
+   * @default -
+   * @required false
+   * @description 抽屉左侧菜单高亮显示
+   */
+  @property()
+  accessor activeKey: string;
 
-    /**
-     * @kind number|string
-     * @required false
-     * @default default
-     * @description 抽屉宽度内容区的宽度，优先级高于bodyStyle内的width
-     * @group basic
-     */
-    @property({
-        attribute: false
-    })
-    accessor width: number | string;
+  /**
+   * @kind number|string
+   * @required false
+   * @default default
+   * @description 抽屉宽度内容区的宽度，优先级高于bodyStyle内的width
+   * @group basic
+   */
+  @property({
+    attribute: false,
+  })
+  accessor width: number | string;
 
-    /**
-     * @kind  React.CSSProperties
-     * @required false
-     * @default default
-     * @description 设计 Drawer 容器样式
-     * @group basic
-     */
-    @property({
-        attribute: false
-    })
-    accessor drawerStyle: React.CSSProperties;
-    /**
-     * @kind  React.CSSProperties
-     * @required false
-     * @default default
-     * @description  可用于设置 Drawer 内容部分的样式
-     * @group basic
-     */
-    @property({
-        attribute: false
-    })
-    accessor bodyStyle: React.CSSProperties;
+  /**
+   * @kind  React.CSSProperties
+   * @required false
+   * @default default
+   * @description 设计 Drawer 容器样式
+   * @group basic
+   */
+  @property({
+    attribute: false,
+  })
+  accessor drawerStyle: React.CSSProperties;
+  /**
+   * @kind  React.CSSProperties
+   * @required false
+   * @default default
+   * @description  可用于设置 Drawer 内容部分的样式
+   * @group basic
+   */
+  @property({
+    attribute: false,
+  })
+  accessor bodyStyle: React.CSSProperties;
 
-    /**
-     * @kind number
-     * @required false
-     * @default  1000
-     * @description 抽屉层级
-     * @group basic
-     */
-    @property({
-        type: Number,
-    })
-    accessor zIndex: number | undefined;
+  /**
+   * @kind number
+   * @required false
+   * @default  1000
+   * @description 抽屉层级
+   * @group basic
+   */
+  @property({
+    type: Number,
+  })
+  accessor zIndex: number | undefined;
 
-    /**
-     * @kind boolean
-     * @required false
-     * @default default
-     * @description 遮罩层是否显示
-     * @group basic
-     */
-    @property({
-        type: Boolean,
-    })
-    accessor visible: boolean | undefined;
-    /**
-     * @detail
-     * @description 抽屉打开事件
-     */
-    @event({ type: "open" })
-    accessor #drawerOpenEvent!: EventEmitter<void>;
+  /**
+   * @kind boolean
+   * @required false
+   * @default default
+   * @description 遮罩层是否显示
+   * @group basic
+   */
+  @property({
+    type: Boolean,
+  })
+  accessor visible: boolean | undefined;
+  /**
+   * @detail
+   * @description 抽屉打开事件
+   */
+  @event({ type: "open" })
+  accessor #drawerOpenEvent!: EventEmitter<void>;
 
-    /**
-     * @detail
-     * @description 抽屉关闭事件
-     */
-    @event({ type: "close" })
-    accessor #drawerCloseEvent!: EventEmitter<void>;
+  /**
+   * @detail
+   * @description 抽屉关闭事件
+   */
+  @event({ type: "close" })
+  accessor #drawerCloseEvent!: EventEmitter<void>;
 
-    /**
-     * @detail `string`
-     * @description 切换 `tab` 栏会触发的事件，`detail` 为目标 `tab` 对应的 `key`
-     */
-    @event({ type: "tab.change"})
-    accessor #tabChangeEvent!: EventEmitter<string>;
+  /**
+   * @detail `string`
+   * @description 切换 `tab` 栏会触发的事件，`detail` 为目标 `tab` 对应的 `key`
+   */
+  @event({ type: "tab.change" })
+  accessor #tabChangeEvent!: EventEmitter<string>;
 
-    #handleTabChange =(key:string)=>{
-        this.activeKey= key;
-        this.#tabChangeEvent.emit(key)
-    }
-    #handleDrawerOpen = () => {
-        this.visible = true;
-        this.#drawerOpenEvent.emit();
-    };
+  #handleTabChange = (key: string) => {
+    this.activeKey = key;
+    this.#tabChangeEvent.emit(key);
+  };
+  #handleDrawerOpen = () => {
+    this.#drawerOpenEvent.emit();
+  };
 
-    #handleDrawerClose = () => {
-        this.visible = false;
-        this.#drawerCloseEvent.emit();
-    };
+  #handleDrawerClose = () => {
+    this.#drawerCloseEvent.emit();
+  };
 
-
-    /**
-     * @description 打开抽屉
-     */
-    @method()
-    open() {
-        this.#handleDrawerOpen();
-    }
-    /**
-     * @description 关闭抽屉
-     */
-    @method()
-    close() {
-        this.#handleDrawerClose();
-    }
-    render(): React.ReactNode {
-        return <TabsDrawerComponent
-                tabList={this.tabList}
-                activeKey={this.activeKey}
-                width={this.width}
-                bodyStyle={this.bodyStyle}
-                drawerStyle={this.drawerStyle}
-                zIndex={this.zIndex}
-                visible={this.visible}
-                onClose={this.#handleDrawerClose}
-                onOpen={this.#handleDrawerOpen}
-                onTabChange={this.#handleTabChange}
-        />
-    }
-
+  /**
+   * @description 打开抽屉
+   */
+  @method()
+  open() {
+    this.visible = true;
+    this.#handleDrawerOpen();
+  }
+  /**
+   * @description 关闭抽屉
+   */
+  @method()
+  close() {
+    this.visible = false;
+    this.#handleDrawerClose();
+  }
+  render(): React.ReactNode {
+    return (
+      <TabsDrawerComponent
+        tabList={this.tabList || []}
+        activeKey={this.activeKey}
+        width={this.width}
+        bodyStyle={this.bodyStyle}
+        drawerStyle={this.drawerStyle}
+        zIndex={this.zIndex}
+        visible={this.visible}
+        onClose={this.#handleDrawerClose}
+        onOpen={this.#handleDrawerOpen}
+        onTabChange={this.#handleTabChange}
+      />
+    );
+  }
 }
-export function TabsDrawerComponent(props: TabsDrawerProps): React.ReactElement {
-    const {tabList=[], width = 378, drawerStyle, bodyStyle, zIndex, visible, onClose, onOpen,onTabChange} = props;
-    const contentRef = useRef<HTMLDivElement>(null);
-    const [activeKey, setActiveKey] = useState(props.activeKey);
-    const setActiveItem = (key: string): void => {
-        const _contentSlot = contentRef.current;
-        if (_contentSlot) {
-            const _slotElement = _contentSlot.querySelectorAll("slot");
-            _slotElement?.forEach((slot) => {
-                slot.hidden = slot.name !== key;
-            });
-            setActiveKey(key);
+export function TabsDrawerComponent(
+  props: TabsDrawerProps
+): React.ReactElement {
+  const {
+    tabList,
+    width = 378,
+    drawerStyle,
+    bodyStyle,
+    zIndex,
+    onClose,
+    onOpen,
+    onTabChange,
+  } = props;
+  const contentRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [activeKey, setActiveKey] = useState(props.activeKey);
+  const [visible, setVisible] = useState<boolean>(props.visible);
+  const setActiveItem = (key: string): void => {
+    const _contentSlot = contentRef.current;
+    if (_contentSlot) {
+      const _slotElement = _contentSlot.querySelectorAll("slot");
+      _slotElement?.forEach((slot) => {
+        slot.hidden = slot.name !== key;
+      });
+      setActiveKey(key);
+    }
+  };
+  const handleCloseClick = () => {
+    visible ? onClose() : onOpen();
+    setVisible(!visible);
+  };
+  const handleClick = useCallback(
+    (event: MouseEvent) => {
+      const targetElement = event
+        .composedPath()
+        .some((ele) => ele === containerRef.current);
+      if (!targetElement) {
+        if (visible) {
+          onClose();
         }
-    };
+        setVisible(false);
+      }
+    },
+    [visible, onClose]
+  );
+  useEffect(() => {
+    if (tabList.length) {
+      setActiveItem(props.activeKey ?? tabList[0].key);
+    }
+  }, [props.activeKey, tabList]);
+  useEffect(() => {
+    setVisible(props.visible);
+  }, [props.visible]);
 
-    useEffect(() => {
-        if (tabList.length) {
-            setActiveItem(props.activeKey ?? tabList[0].key);
-        }
-    }, [props.activeKey, tabList])
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [handleClick]);
 
-    const menuElement = useMemo(() => (
-        <div className="menuWrapper">
-            <div className="menuMask"/>
-            {
-                tabList.map((item, index) => (
-                    <div className={classNames("menuIconItem", {
-                        menuIconItemHover: !!item.tooltip,
-                        active: item.key === activeKey
-                    })}
-                         key={`menu-${index}`}
-                         onClick={() => onTabChange?.(item.key)}
-                    >
-                        {item.icon && <WrappedIcon {...item.icon} />}
-                        {item.tooltip && <div className="menuIconTooltip">{item.tooltip}</div>}
-                    </div>
-                ))
-            }
-
-        </div>), [activeKey, tabList])
-    return (<div className={classNames("drawerWrapper", {
+  const menuElement = useMemo(
+    () => (
+      <div className="menuWrapper">
+        <div className="menuMask" />
+        {tabList.map((item, index) => (
+          <div
+            className={classNames("menuIconItem", {
+              menuIconItemHover: !!item.tooltip,
+              active: item.key === activeKey,
+            })}
+            key={`menu-${index}`}
+            onClick={() => onTabChange?.(item.key)}
+          >
+            {item.icon && <WrappedIcon {...item.icon} />}
+            {item.tooltip && (
+              <div className="menuIconTooltip">{item.tooltip}</div>
+            )}
+          </div>
+        ))}
+      </div>
+    ),
+    [activeKey, onTabChange, tabList]
+  );
+  return (
+    <div
+      className={classNames("drawerWrapper", {
         open: visible,
-        close: !visible
-    })}
-                 style={{
-                     ...drawerStyle,
-                     ...(zIndex ? {zIndex} : {})
-                 }}
+        close: !visible,
+      })}
+      style={{
+        ...drawerStyle,
+        ...(zIndex ? { zIndex } : {}),
+      }}
+      ref={containerRef}
     >
-        <span className="closeIconBtn" onClick={() => visible ? onClose?.() : onOpen?.()}>
-                   <span className="closeIcon"/>
-          </span>
-        <div className={classNames("drawerBody")}
-             style={{
-                 ...bodyStyle,
-                 width,
-             }}
-        >
-
-            {menuElement}
-            <div className="content" ref={contentRef}>
-                {
-                    tabList.map((item, index) => (
-                        <slot name={item.key} key={`slot-${index}`}/>
-                    ))}
-            </div>
+      <span className="closeIconBtn" onClick={handleCloseClick}>
+        <span className="closeIcon" />
+      </span>
+      <div
+        className={classNames("drawerBody")}
+        style={{
+          ...bodyStyle,
+          width,
+        }}
+      >
+        {menuElement}
+        <div className="content" ref={contentRef}>
+          {tabList.map((item, index) => (
+            <slot name={item.key} key={`slot-${index}`} />
+          ))}
         </div>
-
-    </div>)
+      </div>
+    </div>
+  );
 }
 
-export {TabsDrawer}
+export { TabsDrawer };
