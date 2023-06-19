91aca392375fe5dabaad7b6843482aa97aa055c3
weareoutman <wangshenwei@qq.com>
2023-06-19 18:18:39 +0800

    feat(): basic capabilities of code-editor

--- bricks/vs/build.config.js
+++ bricks/vs/build.config.js
@@ -1,3 +1,36 @@
 // @ts-check
+import MonacoWebpackPlugin from "monaco-editor-webpack-plugin";
+
 /** @type {import("@next-core/build-next-bricks").BuildNextBricksConfig} */
-export default {};
+export default {
+  plugins: [
+    new MonacoWebpackPlugin({
+      languages: [
+        "javascript",
+        "typescript",
+        "json",
+        "shell",
+        "powershell",
+        "yaml",
+        "markdown",
+        "python",
+        "java",
+        "xml",
+        "mysql",
+        "go",
+      ],
+      features: [
+        "!accessibilityHelp",
+        "!codelens",
+        "!colorPicker",
+        "!documentSymbols",
+        "!fontZoom",
+        "!iPadShowKeyboard",
+        "!inspectTokens",
+      ],
+      filename: `workers/[name].${
+        process.env.NODE_ENV === "development" ? "bundle" : "[contenthash:8]"
+      }.worker.js`,
+    }),
+  ],
+};
