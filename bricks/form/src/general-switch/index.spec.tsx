4a5265d1cf78e1c18b21d2b09821a3019cfe9b66
zhendonghuang <zhendonghuang@zhendonghuangdeMacBook-Air.local>
2023-06-20 11:40:19 +0800

    feat(): general-switch
    Closes NEXT_BUILDER-3240

---
+++ bricks/form/src/general-switch/index.spec.tsx
@@ -0,0 +1,49 @@
+import { describe, test, expect } from "@jest/globals";
+import { act } from "react-dom/test-utils";
+import "./";
+import { GeneralSwitch } from "./index.js";
+
+jest.mock("@next-core/theme", () => ({}));
+
+describe("form.general-switch", () => {
+  test("basic usage", async () => {
+    const element = document.createElement(
+      "form.general-switch"
+    ) as GeneralSwitch;
+    element.value = false;
+    element.label = "switch";
+    element.disabled = false;
+    element.checkedIcon = {
+      icon: "plus-circle",
+      lib: "antd",
+      theme: "outlined",
+    };
+    element.unCheckedIcon = {
+      icon: "plus-circle",
+      lib: "antd",
+      theme: "outlined",
+    };
+
+    expect(element.shadowRoot).toBeFalsy();
+
+    act(() => {
+      document.body.appendChild(element);
+    });
+    expect(element.shadowRoot?.childNodes.length).toBeGreaterThan(1);
+
+    const mockSwitchEvent = jest.fn();
+    element.addEventListener("switch", mockSwitchEvent);
+    const btn = element.shadowRoot?.querySelector("button") as HTMLElement;
+    await act(async () => {
+      await btn.click();
+    });
+    expect(btn?.className).toBe("switch-checked");
+    expect(mockSwitchEvent).toBeCalledWith(
+      expect.objectContaining({ detail: true })
+    );
+    act(() => {
+      document.body.removeChild(element);
+    });
+    expect(element.shadowRoot?.childNodes.length).toBe(0);
+  });
+});
