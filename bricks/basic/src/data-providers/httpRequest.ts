1c5471e1d76ecc6e98134fbc769f48f785e16570
weareoutman <wangshenwei@qq.com>
2023-06-28 11:47:50 +0800

    fix(): refine types

--- bricks/basic/src/data-providers/httpRequest.ts
+++ bricks/basic/src/data-providers/httpRequest.ts
@@ -1,8 +1,20 @@
 import { createProviderClass } from "@next-core/utils/general";
-import { http } from "@next-core/http";
+import { RequestCustomOptions, http } from "@next-core/http";
 
-export function httpRequest(...args: Parameters<typeof http.request>) {
-  return http.request(...args);
+/**
+ * 发送 HTTP 请求。
+ *
+ * @param url 请求地址
+ * @param init 请求初始化数据
+ * @param options 请求选项
+ * @returns 响应结果
+ */
+export function httpRequest<T = unknown>(
+  url: string,
+  init?: RequestInit,
+  options?: RequestCustomOptions
+): Promise<T> {
+  return http.request<T>(url, init, options);
 }
 
 customElements.define("basic.http-request", createProviderClass(httpRequest));
