3b731af431c25555f585a2597b8bc615e3fc9042
zhouj <zhouj@zhoujdeMacBook-Air.local>
2023-07-11 09:20:49 +0800

    feat():新增大屏框架构件 Closes MONITOR-6466

---
+++ bricks/data-view/src/tech-mesh-base-view/index.spec.tsx
@@ -0,0 +1,31 @@
+import { describe, test, expect, jest } from "@jest/globals";
+import { act } from "react-dom/test-utils";
+import "./";
+import { TechMeshBaseView } from "./index.js";
+
+jest.mock("@next-core/theme", () => ({}));
+
+describe("data-view.tech-mesh-base-view", () => {
+  test("basic usage", async () => {
+    const element = document.createElement(
+      "data-view.tech-mesh-base-view"
+    ) as TechMeshBaseView;
+
+    expect(element.shadowRoot).toBeFalsy();
+
+    act(() => {
+      document.body.appendChild(element);
+    });
+    expect(element.shadowRoot?.childNodes.length).toBeGreaterThan(1);
+
+    expect(element.shadowRoot.querySelectorAll(".layout-wrapper").length).toBe(
+      1
+    );
+    expect(element.shadowRoot.querySelector(".contentWrapper")).toBeTruthy();
+
+    act(() => {
+      document.body.removeChild(element);
+    });
+    expect(element.shadowRoot?.childNodes.length).toBe(0);
+  });
+});
