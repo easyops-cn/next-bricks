2c2465e125d2d1cfbc08fa12b1de07b22db412fe
weareoutman <wangshenwei@qq.com>
2023-08-01 11:28:36 +0800

    chore(): mark shoelace side effects

--- bricks/shoelace/build.config.js
+++ bricks/shoelace/build.config.js
@@ -4,16 +4,27 @@ import CopyPlugin from "copy-webpack-plugin";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const shoelaceMainPath = await import.meta.resolve('@shoelace-style/shoelace');
+const shoelaceMainPath = await import.meta.resolve("@shoelace-style/shoelace");
 
 /** @type {import("@next-core/build-next-bricks").BuildNextBricksConfig} */
 export default {
+  moduleRules: [
+    {
+      test: /[/\\]node_modules[/\\]@shoelace-style[/\\]shoelace[/\\]/,
+      sideEffects: true,
+    },
+  ],
   plugins: [
     new CopyPlugin({
-      patterns: [{
-        from: path.resolve(shoelaceMainPath.replace(/^file:\/\//, ""), "../assets"),
-        to: "assets",
-      }]
-    })
-  ]
+      patterns: [
+        {
+          from: path.resolve(
+            shoelaceMainPath.replace(/^file:\/\//, ""),
+            "../assets"
+          ),
+          to: "assets",
+        },
+      ],
+    }),
+  ],
 };
