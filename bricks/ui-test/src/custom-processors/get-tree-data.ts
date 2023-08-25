9a096fa477f236f81537023e6aa86bb9f4a96c55
jojiang <jojiang@easyops.cn>
2023-08-25 09:44:50 +0800

    fix(): tree data optimization
    Refs NEXT_BUILDER-3898

--- bricks/ui-test/src/custom-processors/get-tree-data.ts
+++ bricks/ui-test/src/custom-processors/get-tree-data.ts
@@ -1,8 +1,6 @@
-// istanbul ignore file
 import { customProcessors } from "@next-core/runtime";
 import type { GeneralIconProps } from "@next-bricks/icons/general-icon";
-import { NodeItem, NodeType } from "../interface.js";
-import { isEmpty } from "lodash";
+import { CommandDoc, NodeType } from "../interface.js";
 
 interface GraphEdge {
   in: string;
@@ -12,6 +10,7 @@ interface GraphEdge {
 
 interface GraphNode {
   name: string;
+  label?: string;
   instanceId: string;
   params?: any;
   type: NodeType;
@@ -30,37 +29,52 @@ interface TreeData {
   data: Record<string, any>;
   children?: TreeData[];
   labelColor?: string;
-  isContainer?: boolean;
+  isChainChild?: boolean;
 }
 
-const setupGroup = ["before", "beforeEach", "after", "afterEach"];
-
-const caseGroup = ["describe", "case"];
-
-const collapseGroups = [...setupGroup, ...caseGroup];
-
-function getIcon(type: string): GeneralIconProps {
+function getIcon(
+  nodeData: GraphNode,
+  commandDocList: CommandDoc[]
+): GeneralIconProps {
   let icon;
   let color;
 
-  if (collapseGroups.includes(type)) {
-    icon = "down";
-    color = "var(--palette-gray-6)";
-  }
+  const doc = commandDocList?.find((item) => item.name === nodeData.name);
 
-  switch (type) {
+  const commandIconMap = {
+    query: {
+      icon: "search",
+      color: "var(--palette-blue-6)",
+    },
+    action: {
+      icon: "tool",
+      color: "var(--palette-red-6)",
+    },
+    assertion: {
+      icon: "aim",
+      color: "var(--palette-green-6)",
+    },
+    other: {
+      icon: "control",
+      color: "var(--palette-green-6)",
+    },
+  };
+
+  switch (nodeData.type) {
     case "suite":
       icon = "file";
       color = "var(--palette-purple-6)";
       break;
-    case "code":
-      icon = "code";
+    case "block":
+      icon = "block";
       color = "var(--palette-cyan-6)";
       break;
-    case "preset":
-      icon = "build";
-      color = "var(--palette-green-6)";
+    case "command": {
+      const result = commandIconMap[doc?.category || "other"];
+      icon = result.icon;
+      color = result.color;
       break;
+    }
   }
 
   return {
@@ -71,54 +85,57 @@ function getIcon(type: string): GeneralIconProps {
   } as GeneralIconProps;
 }
 
-function getRestParams(node: GraphNode): Partial<TreeData> {
-  if (collapseGroups.includes(node.type)) {
-    return {
-      name: node.type,
-      labelColor: "var(--palette-gray-6)",
-      isContainer: true,
-    };
+function getDisplayLabel(nodeItem: GraphNode): string {
+  if (nodeItem.name === "suite") {
+    return nodeItem.name;
+  }
+  if (["describe", "it"].includes(nodeItem.name)) {
+    return `${nodeItem.name}: ${nodeItem.label}`;
   }
 
-  if (["code", "preset"].includes(node.type)) {
-    return {
-      name: node.type,
-    };
-  }
-
-  return {};
+  return nodeItem.name;
 }
 
-export function getTreeData(GraphData: GraphData): TreeData {
+export function getTreeData(
+  GraphData: GraphData,
+  commandDocList: CommandDoc[]
+): TreeData {
   const {
     topic_vertices: [rootData],
     vertices,
     edges,
   } = GraphData;
 
-  const getChildren = (node: GraphNode): TreeData[] => {
+  const getChildren = (
+    node: GraphNode,
+    options: { parentType: NodeType }
+  ): TreeData[] => {
+    const { parentType } = options;
     const relations = edges.filter((item) => item.in === node.instanceId);
 
     const nodes = relations
       .map((relation) => vertices.find((v) => relation.out === v.instanceId))
       .filter(Boolean) as GraphNode[];
 
-    return nodes.map((nodeData) => ({
-      name: nodeData.name,
-      key: nodeData.instanceId,
-      icon: getIcon(nodeData?.type),
-      data: nodeData,
-      children: getChildren(nodeData),
-      ...getRestParams(nodeData),
-    })) as TreeData[];
+    return nodes.map((nodeData) => {
+      const isChainChild =
+        parentType === "command" && nodeData.type === "command";
+      return {
+        name: getDisplayLabel(nodeData),
+        key: nodeData.instanceId,
+        icon: getIcon(nodeData, commandDocList),
+        data: { ...nodeData, isChainChild },
+        children: getChildren(nodeData, { parentType: nodeData.type }),
+      };
+    }) as TreeData[];
   };
 
   return {
-    name: rootData.name,
+    name: getDisplayLabel(rootData),
     key: rootData.instanceId,
-    icon: getIcon(rootData?.type),
+    icon: getIcon(rootData, commandDocList),
     data: rootData,
-    children: getChildren(rootData),
+    children: getChildren(rootData, { parentType: rootData?.type }),
   };
 }
 
--- bricks/ui-test/src/interface.ts
+++ bricks/ui-test/src/interface.ts
@@ -1,18 +1,39 @@
-export type NodeType =
-  | "before"
-  | "beforeEach"
-  | "after"
-  | "afterEach"
-  | "case"
-  | "describe"
-  | "suite"
-  | "preset"
-  | "code";
+export type NodeType = "suite" | "block" | "command";
 
 export interface NodeItem {
-  type: NodeType;
+  type: string;
   children?: NodeItem[];
-  alias?: string;
+  label?: string;
   name?: string;
-  params?: Record<string, any>;
+  params?: any;
+}
+
+export interface CommandParamsField {
+  label: string;
+  description?: string;
+  required?: boolean;
+  default?: any;
+  type:
+    | "number"
+    | "string"
+    | "boolean"
+    | "object"
+    | "array"
+    | "function"
+    | "mixed";
+  enum?: (string | number | EnumItem)[];
+}
+export interface CommandDoc {
+  name: string;
+  description?: string;
+  params?: CommandParamsField[];
+  category: "query" | "action" | "assertion" | "other";
+  from: "cypress" | "third-party" | "custom";
+  chain: "parent" | "child" | "dual";
+  keywords?: string[];
+}
+
+export interface EnumItem {
+  label: string;
+  value: unknown;
 }
