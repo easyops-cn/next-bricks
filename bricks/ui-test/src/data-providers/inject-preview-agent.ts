500898116b356ff6a25f2baebd29c482dab8db9a
weareoutman <wangshenwei@qq.com>
2023-09-04 17:29:12 +0800

    feat(): support recording actions

--- bricks/ui-test/src/data-providers/inject-preview-agent.ts
+++ bricks/ui-test/src/data-providers/inject-preview-agent.ts
@@ -2,12 +2,9 @@
 import { createProviderClass } from "@next-core/utils/general";
 import { getHistory } from "@next-core/runtime";
 import { setPreviewFromOrigin, toggleInspecting } from "./preview/inspector.js";
+import { toggleRecording } from "./preview/recorder.js";
 
 let injected = false;
-
-const EVENTS = ["click", "dblclick", "keydown"] as const;
-let recording = false;
-
 const history = getHistory();
 
 export async function injectPreviewAgent(
@@ -32,14 +29,15 @@ export async function injectPreviewAgent(
   sendMessage({ type: "initialized" });
   setPreviewFromOrigin(previewFromOrigin);
 
+  let inspecting = false;
   window.addEventListener("message", (e) => {
     if (e.data?.channel === "ui-test-preview") {
       switch (e.data.type) {
         case "toggle-record":
-          toggleRecording(e.data.payload.recording);
+          toggleRecording(e.data.payload.recording, inspecting);
           break;
         case "toggle-inspecting":
-          toggleInspecting(e.data.payload.inspecting);
+          toggleInspecting((inspecting = e.data.payload.inspecting));
           break;
         case "reload":
           location.reload();
@@ -72,43 +70,6 @@ export async function injectPreviewAgent(
       },
     });
   });
-}
-
-function onClick(e: Event) {
-  // eslint-disable-next-line no-console
-  console.log(e.type, e);
-  for (const item of e.composedPath()) {
-    if (
-      (item as Node).nodeType === Node.ELEMENT_NODE &&
-      item instanceof HTMLElement &&
-      item.dataset.testid
-    ) {
-      // eslint-disable-next-line no-console
-      console.log("found brick:", item);
-    }
-  }
-}
-
-function toggleRecording(enabled: boolean) {
-  if (recording !== enabled) {
-    recording = enabled;
-    (recording ? startRecord : stopRecord)();
-  }
-}
-
-function startRecord() {
-  for (const type of EVENTS) {
-    document.addEventListener(type, onClick, {
-      capture: true,
-      passive: true,
-    });
-  }
-}
-
-function stopRecord() {
-  for (const type of EVENTS) {
-    document.removeEventListener(type, onClick, { capture: true });
-  }
 }
 
 customElements.define(
