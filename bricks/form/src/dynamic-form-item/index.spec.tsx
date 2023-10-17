8c77de072a558a22ac07463cc4834170cc2ddf09
zhendonghuang <zhendonghuang@zhendonghuangdeMacBook-Air.local>
2023-10-17 15:55:10 +0800

    feat():eo-dynamic-form-item support hideRemoveButton、disabledRemoveButton;hideAddButton、disabledAddButton、message attributes;row.add、row.remove events ref BRICK-69

--- bricks/form/src/dynamic-form-item/index.spec.tsx
+++ bricks/form/src/dynamic-form-item/index.spec.tsx
@@ -49,8 +49,11 @@ describe("form.dynamic-form-item", () => {
       },
     ];
     const mockValueChangeEvent = jest.fn();
+    const mockRowAddEvent = jest.fn();
+    const mockRowRemoveEvent = jest.fn();
     element.addEventListener("change", mockValueChangeEvent);
-
+    element.addEventListener("row.add", mockRowAddEvent);
+    element.addEventListener("row.remove", mockRowRemoveEvent);
     expect(element.shadowRoot).toBeFalsy();
     act(() => {
       document.body.appendChild(element);
@@ -64,6 +67,7 @@ describe("form.dynamic-form-item", () => {
     act(() => {
       (element.shadowRoot?.querySelector(".add-btn") as HTMLElement).click();
     });
+    expect(mockRowAddEvent).toBeCalledTimes(1);
     expect(
       element.shadowRoot?.querySelectorAll(".dynamic-form-item").length
     ).toBe(3);
@@ -73,6 +77,8 @@ describe("form.dynamic-form-item", () => {
         element.shadowRoot?.querySelectorAll(".remove-btn")[1] as HTMLElement
       ).click();
     });
+
+    expect(mockRowAddEvent).toBeCalledTimes(1);
     expect(
       element.shadowRoot?.querySelectorAll(".dynamic-form-item").length
     ).toBe(2);
@@ -244,6 +250,75 @@ describe("form.dynamic-form-item", () => {
       )()
     ).toBe("维度id不能重复");
 
+    act(() => {
+      document.body.removeChild(element);
+    });
+    expect(element.shadowRoot?.childNodes.length).toBe(0);
+  });
+
+  test("hideRemoveButton、disabledRemoveButton;hideAddButton、disabledAddButton", async () => {
+    const element = document.createElement(
+      "form.dynamic-form-item"
+    ) as DynamicFormItem;
+    element.useBrick = [
+      {
+        brick: "form.general-input",
+        properties: {
+          name: "input",
+          required: true,
+          unique: true,
+          validator: jest.fn(),
+        },
+      },
+      {
+        brick: "form.general-select",
+        properties: {
+          name: "select",
+          required: true,
+          options: ["abc", "efg", "hij"],
+          unique: true,
+        },
+      },
+    ];
+
+    element.value = [
+      {
+        input: "hello1",
+        select: "abc1",
+      },
+      {
+        input: "hello2",
+        select: "efg2",
+      },
+      {
+        input: "hello3",
+        select: "efg3",
+      },
+    ];
+
+    element.hideAddButton = (value) => value.length === 5;
+    element.disabledAddButton = (value) => value.length === 4;
+    element.hideRemoveButton = (value, row) => row === 1;
+    element.disabledRemoveButton = (value, row) => row === 0;
+    const mockValueChangeEvent = jest.fn();
+    element.addEventListener("change", mockValueChangeEvent);
+    act(() => {
+      document.body.appendChild(element);
+    });
+
+    expect(
+      element.shadowRoot?.querySelectorAll(".remove-btn-wrapper").length
+    ).toBe(2);
+    expect(
+      element.shadowRoot?.querySelectorAll(".remove-btn-disabled-wrapper")
+        .length
+    ).toBe(1);
+    act(() => {
+      (element.shadowRoot?.querySelector(".add-btn") as HTMLElement).click();
+    });
+    expect(
+      element.shadowRoot?.querySelectorAll(".add-btn.add-btn-disabled").length
+    ).toBe(1);
     act(() => {
       document.body.removeChild(element);
     });
