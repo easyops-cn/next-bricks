10e5437011f2a8e997c8cefad473b0ed2b0568a6
nlicroshan <nlicroshan@easyops.cn>
2023-11-27 15:15:50 +0800

    feat(): eo-launchpad-quick-access
    Closes NEXT_BUILDER-4407

--- bricks/nav/src/bootstrap.ts
+++ bricks/nav/src/bootstrap.ts
@@ -3,3 +3,4 @@ import "./launchpad/index.js";
 import "./launchpad-button-v2/index.js";
 import "./launchpad-config/index.js";
 import "./easyops-navbar-alerts/index.js";
+import "./launchpad-quick-access/index.js";
