2de74ab1c57f5b9439d9fcb290b16679732bd286
SheRunFeng <541899641@qq.com>
2023-12-13 18:40:43 +0800

    chore(form-platform): add stories

---
+++ bricks/form-platform/stories/eo-user-or-user-group-select.ts
@@ -0,0 +1,82 @@
+import {
+  eoUserOrUserGroupSelectBasicSvg,
+  eoUserOrUserGroupSelectBlankListSvg,
+  eoUserOrUserGroupSelectSvg,
+} from "./images";
+
+export const eoUserGroupSelectStory = {
+  storyId: "eo-user-or-user-group-select",
+  text: {
+    en: "user-or-user-group-select",
+    zh: "用户（组）选择器",
+  },
+  description: {
+    en: "support to select user or user group by modal or select",
+    zh: "支持配置选择用户／用户组，下拉框／弹框选择等",
+  },
+  icon: {
+    imgSrc: eoUserOrUserGroupSelectSvg,
+  },
+  conf: [
+    {
+      bricks: [
+        {
+          description: {
+            title: "基本用法",
+          },
+          brick: "eo-user-or-user-group-select",
+          events: {
+            "user.group.change": {
+              action: "console.log",
+            },
+          },
+          properties: {
+            mergeUseAndUserGroup: true,
+            name: "username",
+            staticList: ["easyops"],
+            style: {
+              width: "500px",
+            },
+            value: ["easyops"],
+          },
+        },
+      ],
+      snippetId: "eo-user-or-user-group-select[basic]",
+      title: {
+        en: "Basic User or User Group Select",
+        zh: "基础用户(组)选择器",
+      },
+      thumbnail: eoUserOrUserGroupSelectBasicSvg,
+    },
+    {
+      bricks: [
+        {
+          description: {
+            title: "支持白名单配置",
+          },
+          brick: "eo-user-or-user-group-select",
+          events: {
+            "user.group.change": {
+              action: "console.log",
+            },
+          },
+          properties: {
+            mergeUseAndUserGroup: true,
+            name: "username",
+            staticList: ["easyops"],
+            style: {
+              width: "500px",
+            },
+            value: ["easyops"],
+          },
+        },
+      ],
+      snippetId: "eo-user-or-user-group-select[blank-list]",
+      title: {
+        en: "User or User Group Select with Blank List",
+        zh: "带白名单配置的基础用户(组)选择器",
+      },
+      thumbnail: eoUserOrUserGroupSelectBlankListSvg,
+    },
+  ],
+};
---
+++ bricks/form-platform/stories/index.ts
@@ -0,0 +1,5 @@
+import { eoUserGroupSelectStory } from "./eo-user-or-user-group-select";
+
+const stories = [eoUserGroupSelectStory];
+
+export default stories;
