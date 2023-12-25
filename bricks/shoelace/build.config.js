2ceb657cb1e436f938bcc60209a04283690ad7db
weareoutman <wangshenwei@qq.com>
2023-12-25 11:40:38 +0800

    fix(): transform lit whose output is not ES5 compatible

--- bricks/shoelace/build.config.js
+++ bricks/shoelace/build.config.js
@@ -1,19 +1,39 @@
 // @ts-check
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import CopyPlugin from "copy-webpack-plugin";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const shoelaceMainPath = await import.meta.resolve('@shoelace-style/shoelace');
+const shoelaceMainPath = await import.meta.resolve("@shoelace-style/shoelace");
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import("@next-core/build-next-bricks").BuildNextBricksConfig} */
 export default {
   plugins: [
     new CopyPlugin({
-      patterns: [{
-        from: path.resolve(shoelaceMainPath.replace(/^file:\/\//, ""), "../assets"),
-        to: "assets",
-      }]
-    })
-  ]
+      patterns: [
+        {
+          from: path.resolve(
+            shoelaceMainPath.replace(/^file:\/\//, ""),
+            "../assets"
+          ),
+          to: "assets",
+        },
+      ],
+    }),
+  ],
+  moduleRules: [
+    {
+      test: /\.js$/,
+      include: ["@lit", "lit-element", "lit-html"].map((p) =>
+        path.resolve(__dirname, `../../node_modules/${p}`)
+      ),
+      loader: "babel-loader",
+      options: {
+        rootMode: "upward",
+      },
+    },
+  ],
 };
