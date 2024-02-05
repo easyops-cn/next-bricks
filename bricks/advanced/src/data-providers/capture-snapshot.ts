600f8a23f3a0a591fbc737f7186e4e504dc2df32
astrid <astridli@easyop.cn>
2024-02-05 14:49:44 +0800

    feat(): 新增capture-snapshot provider
    Closes CMDB_VIEW-1001

---
+++ bricks/advanced/src/data-providers/capture-snapshot.ts
@@ -0,0 +1,56 @@
+// istanbul ignore file: nothing logical except calling html2canvas.
+import { createProviderClass } from "@next-core/utils/general";
+import {
+  capture,
+  downloadImage,
+  getCanvasBlob,
+  uploadFile,
+} from "./snapshot.js";
+export type SnapshotOptions = {
+  /** 文件名称 */
+  name: string;
+  /** 文件类型 */
+  fileType: "image" | "pdf";
+  /** 选择器 */
+  selector?: string;
+  /** 对象存储桶名字, fileType只能为 `image` */
+  bucketName?: string;
+};
+/**
+ * 截图，保存或上传
+ *
+ * @param options 选项
+ */
+export async function captureSnapshot(options: SnapshotOptions): Promise<void> {
+  const { name, fileType, bucketName, selector } = options;
+  switch (fileType) {
+    case "image":
+      {
+        const canvas = await capture(selector);
+        if (bucketName) {
+          const blob = await getCanvasBlob(canvas);
+          const files = new File([blob], `${name}.png`, { type: blob.type });
+          await uploadFile(files, bucketName);
+        } else {
+          await downloadImage(canvas, name);
+        }
+      }
+      break;
+    case "pdf": {
+      const { jsPDF } = await import("jspdf");
+      const canvas = await capture(selector);
+      const imageData = canvas.toDataURL("image/jpeg", 1.0);
+      const pdf = new jsPDF("l", "pt", [canvas.width, canvas.height]);
+      pdf.addImage(imageData, "JPEG", 0, 0, canvas.width, canvas.height);
+      await pdf.save(`${name}.pdf`, { returnPromise: true });
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+customElements.define(
+  "advanced.capture-snapshot",
+  createProviderClass(captureSnapshot)
+);
