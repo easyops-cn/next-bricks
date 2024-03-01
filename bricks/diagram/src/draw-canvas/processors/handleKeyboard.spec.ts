1e0f24b94f8adde242a1f56319dd189b262f63cc
weareoutman <wangshenwei@qq.com>
2024-03-01 09:18:28 +0800

    chore(): fix tests

--- bricks/diagram/src/draw-canvas/processors/handleKeyboard.spec.ts
+++ bricks/diagram/src/draw-canvas/processors/handleKeyboard.spec.ts
@@ -1,76 +1,23 @@
 import { describe, test, expect } from "@jest/globals";
 import { handleKeyboard } from "./handleKeyboard";
-import type { RenderedNode } from "../interfaces";
+import type { Cell } from "../interfaces";
 
 describe("handleKeyboard", () => {
-  // Nodes map:
-  //
-  // ```
-  //       a
-  //      ↙ ↘
-  //     b   c
-  //   ↙ ↓ ↘
-  // d   e   f
-  // ```
-  const renderedNodes = [
+  const cells = [
     {
+      type: "node",
       id: "a",
-      data: {
-        id: "a",
-      },
-      x: 150,
-      y: 0,
     },
-    {
-      id: "b",
-      data: {
-        id: "b",
-      },
-      x: 100,
-      y: 100,
-    },
-    {
-      id: "c",
-      data: {
-        id: "c",
-      },
-      x: 200,
-      y: 100,
-    },
-    {
-      id: "d",
-      data: {
-        id: "d",
-      },
-      x: 0,
-      y: 200,
-    },
-    {
-      id: "e",
-      data: {
-        id: "e",
-      },
-      x: 100,
-      y: 200,
-    },
-    {
-      id: "f",
-      data: {
-        id: "f",
-      },
-      x: 200,
-      y: 200,
-    },
-  ] as unknown as RenderedNode[];
+  ] as unknown as Cell[];
 
   test("delete node", () => {
     const action = handleKeyboard(
       new KeyboardEvent("keydown", { key: "Backspace" }),
       {
-        renderedNodes,
+        cells,
         activeTarget: {
           type: "node",
-          nodeId: "a",
+          id: "a",
         },
       }
     );
@@ -80,67 +27,11 @@ describe("handleKeyboard", () => {
     });
   });
 
-  test("delete edge", () => {
-    const action = handleKeyboard(
-      new KeyboardEvent("keydown", { key: "Backspace" }),
-      {
-        renderedNodes,
-        activeTarget: {
-          type: "edge",
-          edge: { source: "a", target: "b" },
-        },
-      }
-    );
-    expect(action).toEqual({
-      action: "delete-edge",
-      edge: { source: "a", target: "b" },
-    });
-  });
-
-  test.each<
-    [activeNodeId: string, key: string, resultNodeId: string | undefined]
-  >([
-    ["a", "ArrowDown", "b"],
-    ["b", "ArrowRight", "c"],
-    ["b", "ArrowUp", "a"],
-    ["c", "ArrowUp", "a"],
-    ["c", "ArrowDown", "f"],
-    ["b", "ArrowDown", "e"],
-    ["f", "ArrowUp", "c"],
-    ["f", "ArrowLeft", "e"],
-  ])("switch active node", (activeNodeId, key, resultNodeId) => {
-    const action = handleKeyboard(new KeyboardEvent("keydown", { key }), {
-      renderedNodes,
-      activeTarget: {
-        type: "node",
-        nodeId: activeNodeId,
-      },
-    });
-    expect(action).toEqual({
-      action: "switch-active-node",
-      node: resultNodeId ? { id: resultNodeId } : undefined,
-    });
-  });
-
-  test("node actions but with active edge", () => {
-    const action = handleKeyboard(
-      new KeyboardEvent("keydown", { key: "ArrowDown" }),
-      {
-        renderedNodes,
-        activeTarget: {
-          type: "edge",
-          edge: { source: "a", target: "b" },
-        },
-      }
-    );
-    expect(action).toBe(undefined);
-  });
-
   test("no active node", () => {
     const action = handleKeyboard(
       new KeyboardEvent("keydown", { key: "Backspace" }),
       {
-        renderedNodes,
+        cells,
         activeTarget: undefined,
       }
     );
@@ -151,10 +42,10 @@ describe("handleKeyboard", () => {
     const action = handleKeyboard(
       new KeyboardEvent("keydown", { key: "Escape" }),
       {
-        renderedNodes,
+        cells,
         activeTarget: {
           type: "node",
-          nodeId: "a",
+          id: "a",
         },
       }
     );
