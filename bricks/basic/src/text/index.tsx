092ab2f4eedcbe5189e2ac8d098c0e70866c42c1
William Cai <williamcai@easyops.cn>
2024-03-04 17:44:45 +0800

    feat(text): add type property
    Closes BRICK-274

--- bricks/basic/src/text/index.tsx
+++ bricks/basic/src/text/index.tsx
@@ -1,8 +1,27 @@
 import React, { CSSProperties } from "react";
 import { createDecorators } from "@next-core/element";
 import { ReactNextElement } from "@next-core/react-element";
+import classNames from "classnames";
+
+import styleText from "./text.shadow.css";
+
+export type TextType =
+  | "secondary"
+  | "success"
+  | "warning"
+  | "danger"
+  | "disabled"
+  | "code"
+  | "keyboard"
+  | undefined;
+
+const typeElementNameMap: Record<string, keyof JSX.IntrinsicElements> = {
+  code: "code",
+  keyboard: "kbd",
+};
 
 export interface TextProps {
+  type?: TextType;
   color?: CSSProperties["color"];
   fontSize?: CSSProperties["fontSize"];
   fontWeight?: CSSProperties["fontWeight"];
@@ -20,9 +39,17 @@ const { defineElement, property } = createDecorators();
  * @category text
  */
 @defineElement("eo-text", {
+  styleTexts: [styleText],
   alias: ["basic.general-text"],
 })
 class Text extends ReactNextElement implements TextProps {
+  /**
+   * 文本类型
+   * @default -
+   */
+  @property()
+  accessor type: TextType;
+
   /**
    * 字体大小
    * @default "14px"
@@ -75,6 +102,7 @@ class Text extends ReactNextElement implements TextProps {
   render() {
     return (
       <TextComponent
+        type={this.type}
         color={this.color}
         fontSize={this.fontSize}
         fontWeight={this.fontWeight}
@@ -87,22 +115,36 @@ class Text extends ReactNextElement implements TextProps {
   }
 }
 export function TextComponent(props: TextProps): React.ReactElement {
+  const {
+    type,
+    color,
+    fontSize,
+    fontWeight,
+    lineHeight,
+    display,
+    textAlign,
+    customStyle,
+  } = props;
+  const TextElementName: keyof JSX.IntrinsicElements =
+    typeElementNameMap[type as string] || "span";
+
   return (
-    <span
+    <TextElementName
+      className={classNames(type)}
       style={
         {
-          color: props.color,
-          fontSize: props.fontSize,
-          fontWeight: props.fontWeight,
-          lineHeight: props.lineHeight,
-          display: props.display,
-          textAlign: props.textAlign,
-          ...props.customStyle,
+          color: color,
+          fontSize: fontSize,
+          fontWeight: fontWeight,
+          lineHeight: lineHeight,
+          display: display,
+          textAlign: textAlign,
+          ...customStyle,
         } as React.CSSProperties
       }
     >
       <slot />
-    </span>
+    </TextElementName>
   );
 }
 export { Text };
