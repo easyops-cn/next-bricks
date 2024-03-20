f987d96bbf1281df0c93b5577aa6ade3b38f4c81
weareoutman <wangshenwei@qq.com>
2024-03-20 15:57:54 +0800

    fix(canvas): parallel lines

--- bricks/diagram/src/diagram/lines/getDirectLinePoints.ts
+++ bricks/diagram/src/diagram/lines/getDirectLinePoints.ts
@@ -1,9 +1,12 @@
-import type { NodePosition, NodeRect } from "../interfaces";
+import type { NodePosition, NodeRect, PositionTuple } from "../interfaces";
 import { doTwoNodesOverlap } from "../processors/doTwoNodesOverlap";
+
+type LineTuple = [start: PositionTuple, end: PositionTuple];
 
 export function getDirectLinePoints(
   source: NodeRect,
-  target: NodeRect
+  target: NodeRect,
+  parallelGap?: number
 ): NodePosition[] | null {
   // Ignore if two nodes are the same.
   // Ignore if two nodes overlap.
@@ -11,38 +14,96 @@ export function getDirectLinePoints(
     return null;
   }
 
-  const dx = target.x - source.x;
-  const dy = target.y - source.y;
+  let p0: PositionTuple;
+  let p1: PositionTuple;
 
-  let x0: number, y0: number, x1: number, y1: number;
-  const directionX = dx > 0 ? 1 : -1;
-  if (dy !== 0) {
-    const deltaRadio = Math.abs(dx / dy);
-    const directionY = dy > 0 ? 1 : -1;
-    const sourceRadio = source.width / source.height;
-    if (deltaRadio < sourceRadio) {
-      x0 = source.x + ((deltaRadio * source.height) / 2) * directionX;
-      y0 = source.y + (source.height / 2) * directionY;
-    } else {
-      x0 = source.x + (source.width / 2) * directionX;
-      y0 = source.y + (source.width / 2 / deltaRadio) * directionY;
-    }
-    const targetRadio = target.width / target.height;
-    if (deltaRadio < targetRadio) {
-      x1 = target.x - ((deltaRadio * target.height) / 2) * directionX;
-      y1 = target.y - (target.height / 2) * directionY;
-    } else {
-      x1 = target.x - (target.width / 2) * directionX;
-      y1 = target.y - (target.width / 2 / deltaRadio) * directionY;
-    }
+  let xDiff = 0;
+  let yDiff = 0;
+
+  if (parallelGap) {
+    const dx = target.x - source.x;
+    const dy = target.y - source.y;
+    const angle = Math.atan2(dy, dx);
+    xDiff = (parallelGap / 2) * Math.cos(angle + Math.PI / 2);
+    yDiff = (parallelGap / 2) * Math.sin(angle + Math.PI / 2);
+  }
+
+  const line: LineTuple = [
+    [source.x + xDiff, source.y + yDiff],
+    [target.x + xDiff, target.y + yDiff],
+  ];
+  const sourceIntersections = getIntersections(source, line);
+  const targetIntersections = getIntersections(target, line);
+
+  // Todo: handle when more than one intersection
+  if (sourceIntersections.length > 0) {
+    p0 = sourceIntersections[0];
   } else {
-    x0 = source.x + (source.width / 2) * directionX;
-    x1 = target.x - (target.width / 2) * directionX;
-    y0 = y1 = source.y;
+    p0 = [source.x, source.y];
+  }
+  if (targetIntersections.length > 0) {
+    p1 = targetIntersections[0];
+  } else {
+    p1 = [target.x, target.y];
   }
 
   return [
-    { x: x0, y: y0 },
-    { x: x1, y: y1 },
+    { x: p0[0], y: p0[1] },
+    { x: p1[0], y: p1[1] },
   ];
+}
+
+function getIntersections(rect: NodeRect, line: LineTuple) {
+  const vertices: PositionTuple[] = [
+    [rect.x - rect.width / 2, rect.y - rect.height / 2],
+    [rect.x + rect.width / 2, rect.y - rect.height / 2],
+    [rect.x + rect.width / 2, rect.y + rect.height / 2],
+    [rect.x - rect.width / 2, rect.y + rect.height / 2],
+  ];
+  const possibleLines: [start: PositionTuple, end: PositionTuple][] = [];
+  for (let i = 0; i < 4; i++) {
+    possibleLines.push([vertices[i], vertices[(i + 1) % 4]]);
+  }
+  const intersections: PositionTuple[] = [];
+  for (const item of possibleLines) {
+    const intersection = intersect(line[0], line[1], item[0], item[1]);
+    if (intersection) {
+      intersections.push(intersection);
+    }
+  }
+  return intersections;
+}
+
+// https://paulbourke.net/geometry/pointlineplane/javascript.txt
+function intersect(
+  [x1, y1]: PositionTuple,
+  [x2, y2]: PositionTuple,
+  [x3, y3]: PositionTuple,
+  [x4, y4]: PositionTuple
+): null | PositionTuple {
+  // Check if none of the lines are of length 0
+  if ((x1 === x2 && y1 === y2) || (x3 === x4 && y3 === y4)) {
+    return null;
+  }
+
+  const denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+
+  // Lines are parallel
+  if (denominator === 0) {
+    return null;
+  }
+
+  const ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denominator;
+  const ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denominator;
+
+  // is the intersection along the segments
+  if (ua < 0 || ua > 1 || ub < 0 || ub > 1) {
+    return null;
+  }
+
+  // Return a object with the x and y coordinates of the intersection
+  const x = x1 + ua * (x2 - x1);
+  const y = y1 + ua * (y2 - y1);
+
+  return [x, y];
 }
