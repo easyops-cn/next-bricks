901e7a1d8ddd572a7ffe105776a837bd639832b8
weareoutman <wangshenwei@qq.com>
2024-05-10 18:39:47 +0800

    fix(): fix auto scroll to bottom

--- bricks/visual-builder/src/chat-conversation/index.tsx
+++ bricks/visual-builder/src/chat-conversation/index.tsx
@@ -231,7 +231,7 @@ export function LegacyChatConversationComponent({
       // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
       containerRef.current?.scrollTo(0, containerRef.current?.scrollHeight!);
     }, 0);
-  }, [storyboard]);
+  }, [messages]);
 
   return (
     <div className="chat" ref={containerRef}>
