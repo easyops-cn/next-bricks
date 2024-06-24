0154d364111dd86d54168d3ab6d2aaa374e7aad3
MJmajiong <936233505@qq.com>
2024-06-24 18:47:48 +0800

    fix(): 修复textarea引入问题 refs BRICK-355

--- bricks/basic/src/editors/eo-textarea/index.tsx
+++ bricks/basic/src/editors/eo-textarea/index.tsx
@@ -3,7 +3,7 @@ import { customEditors } from "@next-core/runtime";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import type { EditorComponentProps } from "@next-bricks/visual-builder/property-editor";
-import { eoTextareaSchema } from "./eoInput.schema";
+import { eoTextareaSchema } from "./eoTextarea.schema";
 
 function EoTextareaComponent(props: EditorComponentProps): React.ReactElement {
   const { SchemaFieldComponent, formilySchemaFormatter, advancedMode } = props;
