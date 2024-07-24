ddfce63ef068769f4dfe8c20561bb8abb3828a25
kehua <kehuaning@easyops.com>
2024-07-24 18:54:02 +0800

    fix(): add v3 table schema
    Ref: BRICK-463

---
+++ bricks/basic/src/editors/eo-next-table/eoNextTable.schema.tsx
@@ -0,0 +1,362 @@
+export const eoNextTableSchema = {
+  name: "layout",
+  type: "void",
+  component: {
+    name: "FormLayout",
+    props: {
+      layout: "vertical",
+    },
+  },
+  children: [
+    {
+      name: "categoryTitle_basic",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "基础",
+        },
+      },
+    },
+    {
+      name: "dataSource",
+      title: "选择数据",
+      type: "string",
+      required: true,
+      component: {
+        name: "Select",
+        props: {
+          placeholder: "请选择数据",
+          allowClear: true,
+        },
+      },
+    },
+    {
+      name: "columns",
+      title: "表格列配置",
+      type: "string",
+      required: true,
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip: "扩展自antd的table Column相关配置项",
+        },
+      },
+    },
+    {
+      type: "string",
+      name: "rowKey",
+      required: true,
+      title: "表格行key",
+      component: {
+        name: "Select",
+        props: {
+          placeholder: "index",
+          allowClear: true,
+        },
+      },
+    },
+    {
+      name: "categoryTitle_search",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "搜索",
+        },
+      },
+    },
+    {
+      name: "frontSearch",
+      title: "使用前端搜索",
+      type: "boolean",
+      decorator: {
+        name: "FormItem",
+        props: {
+          layout: "horizontal",
+        },
+      },
+      "x-reactions": [
+        {
+          target: "searchFields",
+          fulfill: {
+            state: {
+              visible: "{{$self.value}}",
+            },
+          },
+        },
+      ],
+    },
+    {
+      name: "searchFields",
+      title: "搜索目标字段",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip:
+            "支持嵌套的写法,不配置的时候默认为对所有column.dataIndex进行前端搜索",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_page",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "页设置",
+        },
+      },
+    },
+    {
+      name: "isPagination",
+      title: "显示分页",
+      type: "boolean",
+      component: {
+        props: {
+          defaultValue: true,
+        },
+      },
+      "x-reactions": [
+        {
+          target: "pagination",
+          fulfill: {
+            state: {
+              visible: "{{$self.value}}",
+            },
+          },
+        },
+      ],
+    },
+    {
+      name: "pagination",
+      title: "分页配置",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip: "前端搜索时需设置column.sortPriority优先级",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_select",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "选择",
+        },
+      },
+    },
+    {
+      name: "rowSelection",
+      title: "表格行可选配置",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+      },
+    },
+    {
+      name: "selectedRowKeys",
+      title: "指定选中行",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip: "填入key",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_sort",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "排序",
+        },
+      },
+    },
+    {
+      name: "multiSort",
+      title: "支持多列排序",
+      type: "boolean",
+    },
+    {
+      name: "sort",
+      title: "排序配置",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+      },
+    },
+    {
+      name: "categoryTitle_expand",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "展开与树形展示",
+        },
+      },
+    },
+    {
+      name: "expandable",
+      title: "表格行展开配置",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+      },
+    },
+    {
+      name: "expandedRowKeys",
+      title: "展开项的key",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+      },
+    },
+    {
+      name: "childrenColumnName",
+      title: "树形结构列名",
+      type: "string",
+      component: {
+        props: {
+          placeholder: "children",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_style",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "外观",
+        },
+      },
+    },
+    {
+      name: "size",
+      title: "表格大小",
+      type: "string",
+      decorator: {
+        name: "FormItem",
+        props: {
+          layout: "horizontal",
+        },
+      },
+      enum: [
+        {
+          label: "S",
+          value: "small",
+        },
+        {
+          label: "M",
+          value: "middle",
+        },
+        {
+          label: "L",
+          value: "large",
+        },
+      ],
+      component: {
+        name: "Radio.Group",
+        props: {
+          size: "small",
+          optionType: "button",
+          defaultValue: "large",
+        },
+      },
+    },
+    {
+      name: "showHeader",
+      title: "显示表头",
+      type: "boolean",
+      component: {
+        props: {
+          defaultValue: true,
+        },
+      },
+    },
+    {
+      name: "hiddenColumns",
+      title: "隐藏表格列",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip: "填入column.key",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_other",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "其他",
+        },
+      },
+    },
+    {
+      name: "scrollConfig",
+      title: "表格滚动配置",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+      },
+    },
+    {
+      name: "rowDraggable",
+      title: "支持表格行拖拽",
+      type: "boolean",
+      component: {
+        props: {
+          size: "small",
+        },
+      },
+      decorator: {
+        name: "FormItem",
+        props: {
+          layout: "horizontal",
+        },
+      },
+    },
+    {
+      name: "categoryTitle_advanced",
+      type: "void",
+      decorator: {
+        name: "CategoryTitle",
+        props: {
+          text: "高级",
+        },
+      },
+    },
+    {
+      name: "optimizedColumns",
+      title: "需优化渲染的列",
+      type: "string",
+      component: "CodeEditor",
+      decorator: {
+        name: "FormItemWithoutAdvanced",
+        props: {
+          tooltip: "输入对应的column.key",
+        },
+      },
+    },
+  ],
+};
---
+++ bricks/basic/src/editors/eo-next-table/index.tsx
@@ -0,0 +1,134 @@
+import type _React from "react";
+import { customEditors } from "@next-core/runtime";
+import { eoNextTableSchema } from "./eoNextTable.schema";
+import { get, omit } from "lodash";
+
+const transformNormal2Advanced = (rawValue: any) => {
+  const res = {
+    ...omit(rawValue, ["isPagination"]),
+    ...(rawValue.isPagination
+      ? {}
+      : {
+          pagination: false,
+        }),
+  };
+  return res;
+};
+
+const transformAdvanced2Normal = (rawValue: any) => {
+  const paginationConfig = get(rawValue, "pagination");
+  const res = {
+    ...omit(rawValue, ["pagination"]),
+    ...(paginationConfig === false
+      ? {
+          isPagination: false,
+        }
+      : {
+          isPagination: true,
+          pagination: paginationConfig,
+        }),
+  };
+  return res;
+};
+
+function EoNextTableComponentFactory(React: typeof _React) {
+  return function EoNextTableComponent(props: any): React.ReactElement {
+    const {
+      SchemaFieldComponent,
+      formilySchemaFormatter,
+      advancedMode,
+      scope,
+      effects,
+      form,
+    } = props;
+
+    const isInitRef = React.useRef<boolean>(false);
+    const dataListRef = React.useRef<any>();
+
+    React.useEffect(() => {
+      const {
+        onAdvancedChange,
+        onSubmit,
+        onFormInitialValuesChange,
+        onFieldValueChange,
+      } = effects;
+
+      form.setInitialValues({
+        childrenColumnName: "children",
+        size: "large",
+        showHeader: true,
+      });
+
+      // 当data变化时
+      form.addEffects("dataChange", () => {
+        onFormInitialValuesChange(() => {
+          isInitRef.current = true;
+
+          setTimeout(() => {
+            isInitRef.current = false;
+          });
+        }),
+          onFieldValueChange("dataSource", (field: any) => {
+            if (!isInitRef.current) {
+              // 清除关联值
+              form.deleteValuesIn("rowKey");
+            }
+
+            // 修改关联值的options
+            const { value } = field;
+            const matchData = dataListRef.current?.find(
+              (item: any) => item?.value === value
+            );
+            const matchDefinitionOptions =
+              matchData?.dataDefinition?.fields?.map((item: any) => ({
+                label: item?.name,
+                value: item?.name,
+              }));
+            form.query("rowKey").take((field: any) => {
+              field.setComponentProps({
+                options: matchDefinitionOptions,
+              });
+            });
+          });
+      });
+
+      // 监听模式切换
+      form.addEffects("onAdvancedChange", () => {
+        onAdvancedChange((advancedMode: boolean, _: any, values: any) => {
+          return advancedMode
+            ? transformNormal2Advanced(values)
+            : transformAdvanced2Normal(values);
+        });
+      });
+
+      // 监听表单提交
+      form.addEffects("onSubmit", () => {
+        onSubmit((value: any) => {
+          return transformNormal2Advanced(value);
+        });
+      });
+    }, [form]);
+
+    React.useEffect(() => {
+      // 初始化data
+      const { dataList = [] } = scope;
+      dataListRef.current = dataList?.map((item: any) => ({
+        ...item,
+        label: item?.name,
+        value: item?.value,
+      }));
+      form.query("dataSource").take((field: any) => {
+        field.setComponentProps({
+          options: dataListRef.current,
+        });
+      });
+    }, [scope]);
+
+    return React.createElement(SchemaFieldComponent, {
+      schema: formilySchemaFormatter(eoNextTableSchema, advancedMode),
+      scope,
+    });
+  };
+}
+
+customEditors.define("eo-next-table", EoNextTableComponentFactory);
