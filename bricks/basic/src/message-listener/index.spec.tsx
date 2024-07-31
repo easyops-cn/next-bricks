62a31b24f823dea285c2c78ede4694029bfe2e9c
weareoutman <wangshenwei@qq.com>
2024-07-31 17:54:04 +0800

    feat(): new brick: eo-message-listener

---
+++ bricks/basic/src/message-listener/index.spec.tsx
@@ -0,0 +1,96 @@
+import { describe, test, expect, jest } from "@jest/globals";
+import { act } from "react-dom/test-utils";
+import "./";
+import type { EoMessageListener } from "./index.js";
+
+jest.mock("@next-core/theme", () => ({}));
+
+const spyOnAddEventListener = jest.spyOn(window, "addEventListener");
+const spyOnRemoveEventListener = jest.spyOn(window, "removeEventListener");
+
+describe("eo-message-listener", () => {
+  test("basic usage", async () => {
+    const element = document.createElement(
+      "eo-message-listener"
+    ) as EoMessageListener;
+
+    const onMessage = jest.fn();
+    element.addEventListener("message", (e) => {
+      onMessage((e as CustomEvent).detail);
+    });
+
+    act(() => {
+      document.body.appendChild(element);
+    });
+
+    act(() => {
+      // window.postMessage is not implemented in jest-dom
+      (spyOnAddEventListener.mock.calls[0][1] as any)({
+        data: "hello",
+        origin: location.origin,
+      });
+    });
+    expect(onMessage).toBeCalledWith({
+      data: "hello",
+      origin: location.origin,
+    });
+
+    act(() => {
+      (spyOnAddEventListener.mock.calls[0][1] as any)({
+        data: "hi",
+        origin: "http://example.com",
+      });
+    });
+    // not called because of sameOrigin
+    expect(onMessage).toBeCalledTimes(1);
+
+    act(() => {
+      (spyOnAddEventListener.mock.calls[0][1] as any)({
+        data: "world",
+        origin: location.origin,
+      });
+    });
+    expect(onMessage).toBeCalledWith({
+      data: "world",
+      origin: location.origin,
+    });
+
+    act(() => {
+      document.body.removeChild(element);
+    });
+
+    expect(spyOnRemoveEventListener).toBeCalledWith(
+      ...spyOnAddEventListener.mock.calls[0]
+    );
+  });
+  test("disable same origin", async () => {
+    const element = document.createElement(
+      "eo-message-listener"
+    ) as EoMessageListener;
+    element.sameOrigin = false;
+
+    const onMessage = jest.fn();
+    element.addEventListener("message", (e) => {
+      onMessage((e as CustomEvent).detail);
+    });
+
+    act(() => {
+      document.body.appendChild(element);
+    });
+
+    act(() => {
+      (spyOnAddEventListener.mock.calls[0][1] as any)({
+        data: "hello",
+        origin: "http://example.com",
+      });
+    });
+    expect(onMessage).toBeCalledWith({
+      data: "hello",
+      origin: "http://example.com",
+    });
+
+    act(() => {
+      document.body.removeChild(element);
+    });
+  });
+});
---
+++ bricks/basic/src/message-listener/index.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect } from "react";
+import { createDecorators, type EventEmitter } from "@next-core/element";
+import { ReactNextElement } from "@next-core/react-element";
+import "@next-core/theme";
+import styleText from "./styles.shadow.css";
+
+const { defineElement, property, event } = createDecorators();
+
+export interface EoMessageListenerProps {
+  sameOrigin?: boolean;
+}
+
+export interface MessageDetail {
+  data: unknown;
+  origin: string;
+}
+
+/**
+ * 用于监听 window.postMessage 事件的构件
+ */
+export
+@defineElement("eo-message-listener", {
+  styleTexts: [styleText],
+})
+class EoMessageListener
+  extends ReactNextElement
+  implements EoMessageListenerProps
+{
+  /**
+   * 是否仅接收同源消息
+   *
+   * @default true
+   */
+  @property({ type: Boolean })
+  accessor sameOrigin: undefined | boolean;
+
+  @event({ type: "message" })
+  accessor #messageEvent!: EventEmitter<MessageDetail>;
+
+  #handleMessage = (detail: MessageDetail) => {
+    this.#messageEvent.emit(detail);
+  };
+
+  render() {
+    return (
+      <EoMessageListenerComponent
+        sameOrigin={this.sameOrigin}
+        onMessage={this.#handleMessage}
+      />
+    );
+  }
+}
+
+export interface EoMessageListenerComponentProps
+  extends EoMessageListenerProps {
+  onMessage(detail: MessageDetail): void;
+}
+
+export function EoMessageListenerComponent({
+  sameOrigin: _sameOrigin,
+  onMessage,
+}: EoMessageListenerComponentProps) {
+  const sameOrigin = _sameOrigin ?? true;
+
+  useEffect(() => {
+    function callback(event: MessageEvent) {
+      if (!sameOrigin || event.origin === location.origin) {
+        onMessage({ data: event.data, origin: event.origin });
+      }
+    }
+    window.addEventListener("message", callback);
+    return () => {
+      window.removeEventListener("message", callback);
+    };
+  }, [onMessage, sameOrigin]);
+  return null;
+}
