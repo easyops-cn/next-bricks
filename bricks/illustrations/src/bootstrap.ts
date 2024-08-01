890d3e45ede83f37476e3da3955c052153edd849
weareoutman <wangshenwei@qq.com>
2024-08-01 21:07:25 +0800

    feat(): new brick: rocket-transition

--- bricks/illustrations/src/bootstrap.ts
+++ bricks/illustrations/src/bootstrap.ts
@@ -1,2 +1,3 @@
 import "./data-providers/get-illustration.js";
 import "./data-providers/get-illustrations-by-category.js";
+import "./rocket-transition/index.js";
---
+++ bricks/illustrations/src/rocket-transition/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createDecorators } from "@next-core/element";
+import { ReactNextElement } from "@next-core/react-element";
+import styleText from "./styles.shadow.css";
+
+const { defineElement } = createDecorators();
+
+/**
+ * 构件 `illustrations.rocket-transition`
+ */
+export
+@defineElement("illustrations.rocket-transition", {
+  styleTexts: [styleText],
+})
+class RocketTransition extends ReactNextElement {
+  render() {
+    return <RocketTransitionComponent />;
+  }
+}
+
+export function RocketTransitionComponent() {
+  return (
+    <>
+      <div className="rocket">
+        <div className="ring">
+          <div className="ball-1"></div>
+          <div className="ball-2"></div>
+        </div>
+      </div>
+      <div className="diamond-1"></div>
+      <div className="diamond-2"></div>
+      <div className="diamond-3"></div>
+    </>
+  );
+}
