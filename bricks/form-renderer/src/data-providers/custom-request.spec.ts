0f6838a8cc4b17b735711e10946c23837b3f1e24
李佳明 <jimmyli@easyops.cn>
2024-11-14 17:19:53 +0800

    feat(): 支持form-data格式的请求
    Closes SHLSTYYWPT-684

--- bricks/form-renderer/src/data-providers/custom-request.spec.ts
+++ bricks/form-renderer/src/data-providers/custom-request.spec.ts
@@ -13,6 +13,18 @@ describe("customRequest", () => {
     );
   });
 
+  test("should work with form-data", async () => {
+    const mockHttp = jest.spyOn(http, "request");
+    await customRequest(
+      "api/cmdb",
+      { method: "POST", body: { a: 1 } },
+      { requestType: "form-data" }
+    );
+    expect(mockHttp.mock.calls[0][0]).toEqual(
+      "api/gateway/logic.gateway_serviceapi/cmdb"
+    );
+  });
+
   test("should work with full url", async () => {
     const mockHttp = jest.spyOn(http, "request");
     await customRequest("https://devops.dev.com/api/cmdb", { method: "GET" });
--- bricks/form-renderer/src/data-providers/custom-request.ts
+++ bricks/form-renderer/src/data-providers/custom-request.ts
@@ -1,17 +1,54 @@
 import { createProviderClass } from "@next-core/utils/general";
 import { RequestCustomOptions, http } from "@next-core/http";
+import { isObject } from "lodash";
 
 /**
  * 用于 form-builder 第三方转发请求
  */
+
+export type FormRendererCustomRequests = RequestCustomOptions & {
+  requestType: "json" | "form-data";
+};
+
+export function transformFormData(data: FormData | BodyInit): FormData {
+  if (data instanceof FormData) {
+    return data;
+  }
+
+  const formData = new FormData();
+  for (const [key, value] of Object.entries(data)) {
+    if (Array.isArray(value)) {
+      value.forEach((v) => {
+        formData.append(key, v);
+      });
+    } else if (
+      isObject(value) &&
+      !(value instanceof Blob) &&
+      !(value instanceof Date)
+    ) {
+      Object.entries(value).forEach(([k, v]) => {
+        formData.append(`${key}[${k}]`, v);
+      });
+    } else {
+      formData.append(key, value as string);
+    }
+  }
+
+  return formData;
+}
 export async function customRequest(
   url: string,
   init?: RequestInit,
-  options?: RequestCustomOptions
+  options?: FormRendererCustomRequests
 ): Promise<unknown> {
   const prefix = "api/gateway/logic.gateway_service";
 
   const finalUrl = /^https?:/.test(url) ? url : `${prefix}${url}`;
+
+  if (options?.requestType === "form-data" && init?.body) {
+    init.body = transformFormData(init.body);
+  }
+
   return http.request(finalUrl, init, options);
 }
 
