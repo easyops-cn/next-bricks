8f8843fd576a53b73fc5aa19d6f66f3d00c434c9
weareoutman <wangshenwei@qq.com>
2024-12-30 10:46:54 +0800

    feat(eo-carousel-text): use speed instead of duration

--- bricks/presentational/src/carousel-text/index.spec.tsx
+++ bricks/presentational/src/carousel-text/index.spec.tsx
@@ -5,22 +5,70 @@ import type { EoCarouselText } from "./index.js";
 
 jest.mock("@next-core/theme", () => ({}));
 
+jest.mock(
+  "resize-observer-polyfill",
+  () =>
+    class {
+      #callback: ResizeObserverCallback;
+      constructor(callback: ResizeObserverCallback) {
+        this.#callback = callback;
+      }
+      disconnect() {}
+      observe() {
+        Promise.resolve().then(() => {
+          this.#callback([], this);
+        });
+      }
+      unobserve() {}
+    }
+);
+
 describe("eo-carousel-text", () => {
   test("basic usage", async () => {
     const element = document.createElement(
       "eo-carousel-text"
     ) as EoCarouselText;
 
-    expect(element.shadowRoot).toBeFalsy();
-
     act(() => {
       document.body.appendChild(element);
     });
-    expect(element.shadowRoot?.childNodes.length).toBeGreaterThan(1);
+
+    const container = element.shadowRoot.querySelector(
+      ".container"
+    ) as HTMLElement;
+    Object.defineProperty(container, "offsetWidth", {
+      value: 400,
+      writable: true,
+    });
+
+    Object.defineProperty(
+      element.shadowRoot.querySelector(".scroll") as HTMLElement,
+      "offsetWidth",
+      {
+        value: 600,
+        writable: true,
+      }
+    );
+
+    expect(container.classList.contains("ready")).toBe(false);
+
+    await act(async () => {
+      await (global as any).flushPromises();
+    });
+
+    expect(container.classList.contains("ready")).toBe(true);
+    expect(
+      (container as HTMLElement).style.getPropertyValue("--transform-start")
+    ).toBe("translateX(400px)");
+    expect(
+      (container as HTMLElement).style.getPropertyValue("--transform-end")
+    ).toBe("translateX(-600px)");
+    expect(
+      (container as HTMLElement).style.getPropertyValue("--transform-duration")
+    ).toBe("10s");
 
     act(() => {
       document.body.removeChild(element);
     });
-    expect(element.shadowRoot?.childNodes.length).toBe(0);
   });
 });
--- bricks/presentational/src/carousel-text/index.tsx
+++ bricks/presentational/src/carousel-text/index.tsx
@@ -1,8 +1,10 @@
-import React, { CSSProperties } from "react";
+import React, { CSSProperties, useEffect, useRef } from "react";
 import { createDecorators } from "@next-core/element";
 import { ReactNextElement } from "@next-core/react-element";
 import "@next-core/theme";
+import ResizeObserver from "resize-observer-polyfill";
 import styleText from "./styles.shadow.css";
+import { useState } from "react";
 
 const { defineElement, property } = createDecorators();
 
@@ -25,34 +27,34 @@ class EoCarouselText extends ReactNextElement {
    * @default "100%"
    */
   @property()
-  accessor containerWidth: CSSProperties["width"] = "100%";
+  accessor containerWidth: CSSProperties["width"];
 
   /**
    * 字体大小
-   * @default "14px"
+   * @default "var(--normal-font-size)"
    */
   @property()
-  accessor fontSize: CSSProperties["fontSize"] = "14px";
+  accessor fontSize: CSSProperties["fontSize"];
 
   /**
    * 字体颜色
-   * @default "black"
+   * @default "var(--text-color-default)"
    */
   @property()
-  accessor fontColor: CSSProperties["color"] = "black";
+  accessor fontColor: CSSProperties["color"];
 
   /**
-   * 动画周期所需时间，单位ms
-   * @default 3000
+   * 移动速度，单位 px/s
+   * @default 100
    */
   @property()
-  accessor animationDuration: number = 3000;
+  accessor speed: number;
 
   render() {
     return (
       <EoCarouselTextComponent
         text={this.text}
-        animationDuration={this.animationDuration}
+        speed={this.speed}
         fontColor={this.fontColor}
         fontSize={this.fontSize}
         containerWidth={this.containerWidth}
@@ -63,24 +65,71 @@ class EoCarouselText extends ReactNextElement {
 
 export interface EoCarouselTextProps {
   text: string;
-  animationDuration: number;
+  speed?: number;
   fontColor: CSSProperties["color"];
   fontSize: CSSProperties["fontSize"];
   containerWidth: CSSProperties["width"];
 }
 
-export function EoCarouselTextComponent(props: EoCarouselTextProps) {
-  const { text, animationDuration, fontColor, fontSize, containerWidth } =
-    props;
+export function EoCarouselTextComponent({
+  text,
+  fontColor,
+  fontSize,
+  containerWidth,
+  speed: _speed,
+}: EoCarouselTextProps) {
+  const speed = _speed ?? 100;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(0);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const scroll = scrollRef.current;
+
+    if (!container || !scroll) {
+      return;
+    }
+    const observer = new ResizeObserver(() => {
+      setStart(container.offsetWidth);
+      setEnd(-scroll.offsetWidth);
+    });
+
+    observer.observe(container);
+    observer.observe(scroll);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  const ready = start && end;
+
   return (
-    <div className="scrollContainer" style={{ width: containerWidth }}>
+    <div
+      className={`container${ready ? " ready" : ""}`}
+      style={
+        {
+          width: containerWidth,
+          ...(ready
+            ? {
+                "--transform-start": `translateX(${start}px)`,
+                "--transform-end": `translateX(${end}px)`,
+                "--transform-duration": `${Math.abs(end - start) / speed}s`,
+              }
+            : null),
+        } as any
+      }
+      ref={containerRef}
+    >
       <div
-        className="scrollText"
+        className="scroll"
         style={{
-          animationDuration: `${animationDuration}ms`,
           color: fontColor,
           fontSize: fontSize,
         }}
+        ref={scrollRef}
       >
         {text}
       </div>
