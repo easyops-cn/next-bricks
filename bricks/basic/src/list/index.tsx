62bacaa41b39f4b2f8e61d16dafea30dcd97c0bf
weareoutman <wangshenwei@qq.com>
2025-07-11 19:20:50 +0800

    fix(list): add two colors

--- bricks/basic/src/list/index.tsx
+++ bricks/basic/src/list/index.tsx
@@ -107,7 +107,7 @@ function DefaultItem({ title, icon, url, href, variant }: ItemProps) {
   const color = useMemo(() => {
     const palette =
       variant === "navigation"
-        ? new Array(17).fill(0).map((_, i) => `color-${i + 1}`)
+        ? new Array(19).fill(0).map((_, i) => `color-${i + 1}`)
         : ["blue", "orange", "purple", "green", "cyan"];
     // Get color index by accumulate the character code of all letters of the title
     const colorIndex =
